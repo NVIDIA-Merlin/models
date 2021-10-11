9fec2acf9a44ed3ae972d6dd430f155446ba5e86
Marc Romeyn <marcromeyn@gmail.com>
2021-10-11 13:08:54 +0200

    Updating setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,52 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-import setuptools
 
-setuptools.setup()
+import codecs
+import itertools
+import os
+
+from setuptools import find_packages, setup
+
+import versioneer
+
+
+def read_requirements(filename):
+    base = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(base, filename), "rb", "utf-8") as f:
+        lineiter = (line.strip() for line in f)
+        return [line for line in lineiter if line and not line.startswith("#")]
+
+
+requirements = {
+    "base": read_requirements("requirements/base.txt"),
+    "tensorflow": read_requirements("requirements/tensorflow.txt"),
+    "pytorch": read_requirements("requirements/pytorch.txt"),
+    "nvtabular": read_requirements("requirements/nvtabular.txt"),
+    "dev": read_requirements("requirements/dev.txt"),
+}
+
+
+setup(
+    name="merlin-models",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    url="https://github.com/NVIDIA-Merlin/models",
+    author="NVIDIA Corporation",
+    license="Apache 2.0",
+    long_description=open("README.md", encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    install_requires=requirements["base"],
+    test_suite="tests",
+    extras_require={**requirements, "all": list(itertools.chain(*list(requirements.values())))},
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering",
+    ],
+)
