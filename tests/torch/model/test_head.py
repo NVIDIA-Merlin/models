81572dcb5280dc5fa0c8d770d7bd07a9f389d0ed
Marc Romeyn <marcromeyn@gmail.com>
2021-11-24 12:44:42 +0100

    Quick-fix to remove transformers import

--- tests/torch/model/test_head.py
+++ tests/torch/model/test_head.py
@@ -35,15 +35,13 @@ def test_simple_heads(torch_tabular_features, torch_tabular_data, task):
 
 @pytest.mark.parametrize("task", [tr.BinaryClassificationTask, tr.RegressionTask])
 @pytest.mark.parametrize("task_block", [None, tr.MLPBlock([32]), tr.MLPBlock([32]).build([-1, 64])])
-@pytest.mark.parametrize("summary", ["last", "first", "mean", "cls_index"])
-def test_simple_heads_on_sequence(
-    torch_tabular_features, torch_yoochoose_like, task, task_block, summary
-):
+# @pytest.mark.parametrize("summary", ["last", "first", "mean", "cls_index"])
+def test_simple_heads_on_sequence(torch_tabular_features, torch_yoochoose_like, task, task_block):
     inputs = torch_tabular_features
     targets = {"target": pytorch.randint(2, (100,)).float()}
 
     body = tr.SequentialBlock(inputs, tr.MLPBlock([64]))
-    head = task("target", task_block=task_block, summary_type=summary).to_head(body, inputs)
+    head = task("target", task_block=task_block).to_head(body, inputs)
 
     body_out = body(torch_yoochoose_like)
     loss = head.compute_loss(body_out, targets)
