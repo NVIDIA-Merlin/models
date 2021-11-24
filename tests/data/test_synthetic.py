8440febe3bf0cb999e80b3647e7942c609649e7b
sararb <sara.rabhi@gmail.com>
2021-11-24 08:28:30 -0500

    add unit test of synthetic generation

--- tests/data/test_synthetic.py
+++ tests/data/test_synthetic.py
@@ -1,11 +1,9 @@
 import pytest
 
-from merlin_models.data.synthetic import (
-    generate_recsys_data,
-    synthetic_retrieval_schema,
-)
+from merlin_models.data.synthetic import generate_recsys_data, synthetic_retrieval_schema
 
 pd = pytest.importorskip("pandas")
+
 
 def test_generate_item_interactions():
     data = generate_recsys_data(500, synthetic_retrieval_schema)
@@ -13,23 +11,23 @@ def test_generate_item_interactions():
     assert isinstance(data, pd.DataFrame)
     assert len(data) == 500
     assert list(data.columns) == [
-        'session_id',
-        'purchase',
-        'price',
-        'item_id',
-        'category',
-        'item_recency', 
-        'genres',
-        'user_id', 
-        'age', 
-        'sex', 
+        "session_id",
+        "purchase",
+        "price",
+        "item_id",
+        "category",
+        "item_recency",
+        "genres",
+        "user_id",
+        "age",
+        "sex",
     ]
     expected_dtypes = {
         "session_id": "int64",
         "item_id": "int64",
         "user_id": "int64",
         "age": "float64",
-        "sex": "int64", 
+        "sex": "int64",
         "genres": "int64",
         "category": "int64",
         "item_recency": "float64",
@@ -37,4 +35,6 @@ def test_generate_item_interactions():
         "price": "float64",
     }
 
-    assert all(val == expected_dtypes[key] for key, val in dict(data.dtypes).items() if key != 'genres')
+    assert all(
+        val == expected_dtypes[key] for key, val in dict(data.dtypes).items() if key != "genres"
+    )
