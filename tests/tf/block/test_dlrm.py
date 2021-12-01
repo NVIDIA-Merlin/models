2df4f2fd6234e7cb237775b9fd14e75dcec133cd
Gabriel Moreira <gmoreira@nvidia.com>
2021-11-30 21:36:56 -0300

    Added unit tests for DLRM

--- tests/tf/block/test_dlrm.py
+++ tests/tf/block/test_dlrm.py
@@ -17,14 +17,60 @@
 import pytest
 
 from merlin_models.data.synthetic import SyntheticData
+from merlin_standard_lib import Tag
 
 tr = pytest.importorskip("merlin_models.tf")
 
 
 def test_dlrm_block_yoochoose(testing_data: SyntheticData):
+
     dlrm = tr.DLRMBlock(
-        testing_data.schema, bottom_block=tr.MLPBlock([64]), top_block=tr.MLPBlock([64])
+        testing_data.schema, bottom_block=tr.MLPBlock([64]), top_block=tr.MLPBlock([32])
     )
     outputs = dlrm(testing_data.tf_tensor_dict)
 
-    assert list(outputs.shape) == [100, 64]
+    assert list(outputs.shape) == [100, 32]
+
+
+def test_dlrm_block_no_continuous_features(testing_data):
+    schema = testing_data.schema.remove_by_tag(Tag.CONTINUOUS)
+    dlrm = tr.DLRMBlock(schema, bottom_block=tr.MLPBlock([64]), top_block=tr.MLPBlock([32]))
+    outputs = dlrm(testing_data.tf_tensor_dict)
+
+    assert list(outputs.shape) == [100, 32]
+
+
+def test_dlrm_block_no_categ_features(testing_data):
+    schema = testing_data.schema.remove_by_tag(Tag.CATEGORICAL)
+    dlrm = tr.DLRMBlock(schema, bottom_block=tr.MLPBlock([64]), top_block=tr.MLPBlock([32]))
+    outputs = dlrm(testing_data.tf_tensor_dict)
+
+    assert list(outputs.shape) == [100, 32]
+
+
+def test_dlrm_block_single_categ_feature(testing_data):
+    schema = testing_data.schema.select_by_tag(Tag.ITEM_ID)
+    dlrm = tr.DLRMBlock(schema, bottom_block=tr.MLPBlock([64]), top_block=tr.MLPBlock([32]))
+    outputs = dlrm(testing_data.tf_tensor_dict)
+
+    assert list(outputs.shape) == [100, 32]
+
+
+def test_dlrm_block_single_continuous_feature(testing_data):
+    schema = testing_data.schema.select_by_name("event_hour_sin")
+    dlrm = tr.DLRMBlock(schema, bottom_block=tr.MLPBlock([64]), top_block=tr.MLPBlock([32]))
+    outputs = dlrm(testing_data.tf_tensor_dict)
+
+    assert list(outputs.shape) == [100, 32]
+
+
+def test_dlrm_block_no_schema():
+    with pytest.raises(ValueError) as excinfo:
+        tr.DLRMBlock(schema=None, bottom_block=tr.MLPBlock([64]), top_block=tr.MLPBlock([32]))
+    assert "The schema is required by DLRM" in str(excinfo.value)
+
+
+def test_dlrm_block_no_bottom_block(tabular_schema):
+    with pytest.raises(ValueError) as excinfo:
+        tr.DLRMBlock(schema=tabular_schema, bottom_block=None)
+    assert "The bottom_block is required by DLRM" in str(excinfo.value)
