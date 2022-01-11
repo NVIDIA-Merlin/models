de0b6c3e92d32f1ea37b947a98d4f9924951e2a4
sararb <sara.rabhi@gmail.com>
2022-01-11 09:04:22 -0500

    update tests of masking and youtube-dnn model

--- tests/tf/block/test_masking.py
+++ tests/tf/block/test_masking.py
@@ -19,43 +19,21 @@ import pytest
 from merlin_models.data.synthetic import SyntheticData
 from merlin_standard_lib.utils.proto_utils import has_field
 
+tf = pytest.importorskip("tensorflow")
 ml = pytest.importorskip("merlin_models.tf")
 
 
-def test_masking_block(sequence_testing_data: SyntheticData):
+@pytest.mark.parametrize("mask_block", [ml.CausalLanguageModeling(), ml.MaskedLanguageModeling()])
+def test_masking_block(sequence_testing_data: SyntheticData, mask_block):
     list_features = [
         f.name for f in sequence_testing_data.schema.feature if has_field(f, "value_count")
     ]
+
     schema_list = sequence_testing_data.schema.select_by_name(list_features)
     embedding_block = ml.InputBlock(schema_list, aggregation="concat", seq=True)
-
-    mask_block = ml.CausalLanguageModeling()
 
     model = embedding_block.connect(mask_block, context=ml.BlockContext())
 
     batch = sequence_testing_data.tf_tensor_dict
     masked_input = model(batch)
     assert masked_input.shape[-1] == 148
-
-
-@pytest.mark.parametrize("run_eagerly", [True, False])
-def test_masking_head(sequence_testing_data: SyntheticData, run_eagerly: bool):
-    list_features = [
-        f.name for f in sequence_testing_data.schema.feature if has_field(f, "value_count")
-    ]
-    schema_list = sequence_testing_data.schema.select_by_name(list_features)
-
-    embedding_block = ml.InputBlock(schema_list, aggregation="concat", seq=True)
-    mask_block = ml.CausalLanguageModeling(train_on_last_item_seq_only=True, combiner="mean")
-    task = ml.prediction.item_prediction.NextItemPredictionTask(sequence_testing_data.schema)
-
-    model = embedding_block.connect(mask_block, ml.MLPBlock([64]), task, context=ml.BlockContext())
-    model.compile(optimizer="adam", run_eagerly=run_eagerly)
-    out = model(sequence_testing_data.tf_tensor_dict)
-    losses = model.fit(sequence_testing_data.tf_dataloader(batch_size=100), epochs=2)
-
-    assert len(losses.epoch) == 2
-    for metric in losses.history.keys():
-        assert type(losses.history[metric]) is list
-    assert len(losses.history[metric]) == 2
-    assert out.shape[-1] == 51997
--- tests/tf/prediction/test_item_prediction.py
+++ tests/tf/prediction/test_item_prediction.py
@@ -19,6 +19,7 @@ import pytest
 import merlin_models.tf as ml
 from merlin_models.data.synthetic import SyntheticData
 from merlin_standard_lib import Tag
+from merlin_standard_lib.utils.proto_utils import has_field
 
 
 @pytest.mark.parametrize("run_eagerly", [True, False])
@@ -34,3 +35,27 @@ def test_retrieval_task(music_streaming_data: SyntheticData, run_eagerly, num_ep
     losses = model.fit(music_streaming_data.tf_dataloader(batch_size=50), epochs=num_epochs)
     assert len(losses.epoch) == num_epochs
     assert all(measure >= 0 for metric in losses.history for measure in losses.history[metric])
+
+
+@pytest.mark.parametrize("run_eagerly", [True, False])
+def test_youtube_dnn(sequence_testing_data: SyntheticData, run_eagerly: bool):
+    list_features = [
+        f.name for f in sequence_testing_data.schema.feature if has_field(f, "value_count")
+    ]
+    schema_list = sequence_testing_data.schema.select_by_name(list_features)
+
+    embedding_block = ml.InputBlock(schema_list, aggregation="concat", seq=True)
+    mask_block = ml.CausalLanguageModeling(train_on_last_item_seq_only=True, combiner="mean")
+    task = ml.NextItemPredictionTask(sequence_testing_data.schema)
+
+    model = embedding_block.connect(mask_block, ml.MLPBlock([64]), task, context=ml.BlockContext())
+    model.compile(optimizer="adam", run_eagerly=run_eagerly)
+    losses = model.fit(sequence_testing_data.tf_dataloader(batch_size=50), epochs=2)
+
+    assert len(losses.epoch) == 2
+    for metric in losses.history.keys():
+        assert type(losses.history[metric]) is list
+    assert len(losses.history[metric]) == 2
+
+    out = model(sequence_testing_data.tf_tensor_dict)
+    assert out.shape[-1] == 51997
