Fixing bug on pre-sorted metrics that had invalid (truncated) relevant counts. Start refactoring metrics computation

merlin/models/tf/core.py
@@ -17,6 +17,7 @@
 import abc
 import copy
 import sys
+from collections import Sequence as SequenceCollection
 from collections import defaultdict
 from dataclasses import dataclass
 from functools import reduce
@@ -50,11 +51,11 @@ from merlin.models.utils.schema import (
 )
 from merlin.schema import Schema, Tags
 
+from .metrics.ranking import RankingMetric2
 from .typing import TabularData, TensorOrTabularData
 from .utils.mixins import LossMixin, MetricsMixin, ModelLikeBlock
 from .utils.tf_utils import (
     calculate_batch_size_from_input_shapes,
-    extract_topk,
     maybe_deserialize_keras_objects,
     maybe_serialize_keras_objects,
 )
@@ -1756,6 +1757,12 @@ class PredictionTask(Layer, LossMixin, MetricsMixin, ContextMixin):
         self.pre = pre
         self.pre_eval_topk = pre_eval_topk
 
+        if metrics is not None:
+            if isinstance(metrics, SequenceCollection):
+                metrics = list(metrics)
+            else:
+                metrics = [metrics]
+
         create_metrics = self._create_metrics
         self.eval_metrics = create_metrics(metrics) if metrics else []
         self.prediction_metrics = create_metrics(prediction_metrics) if prediction_metrics else []
@@ -1843,6 +1850,7 @@ class PredictionTask(Layer, LossMixin, MetricsMixin, ContextMixin):
             predictions, targets=targets, sample_weight=sample_weight, training=training
         )
 
+        update_ops = []
         if compute_metrics:
             update_ops = self.calculate_metrics(
                 predictions, targets, loss=loss, forward=False, training=training
@@ -1875,30 +1883,52 @@ class PredictionTask(Layer, LossMixin, MetricsMixin, ContextMixin):
         if forward:
             predictions = self(predictions)
 
-        metrics = (
-            self.eval_metrics + self.prediction_metrics + self.label_metrics + self.loss_metrics
-        )
-
-        ranking_metrics = list([metric for metric in metrics if hasattr(metric, "top_ks")])
-        if training:
-            if len(ranking_metrics) > 0:
-                # Sorts the top-k items for metrics computation
-                max_k = tf.reduce_max([metric.top_ks for metric in ranking_metrics])
-                predictions, targets = extract_topk(max_k, predictions, targets)
-
-        else:
-            if self.pre_eval_topk:
-                outputs = self.pre_eval_topk.call_targets(
-                    PredictionOutput(predictions, targets), **kwargs
-                )
-                targets, predictions = outputs.targets, outputs.predictions
-
         update_ops = []
 
-        for metric in self.eval_metrics:
-            update_ops.append(
-                metric.update_state(y_true=targets, y_pred=predictions, sample_weight=sample_weight)
+        if not training and self.pre_eval_topk:
+            outputs = self.pre_eval_topk.call_targets(
+                PredictionOutput(predictions, targets), **kwargs
             )
+            targets, predictions = outputs.targets, outputs.predictions
+            relevant_count = None
+
+        if len(self.eval_metrics) > 0:
+            predictions_eval = predictions
+            targets_eval = targets
+
+            if training:
+                ranking_metrics = list(
+                    [metric for metric in self.eval_metrics if isinstance(metric, RankingMetric2)]
+                )
+
+                if len(ranking_metrics) > 0:
+                    # Checking the highest k used in ranking metrics
+                    max_k = tf.reduce_max([metric.k for metric in ranking_metrics])
+                    # Pre-sorts predictions and targets (by prediction scores) only once
+                    # for all ranking metric (for performance optimization) and extracts
+                    # only the top-k predictions/targets.
+                    # The relevant_count is necessary because when extracing the labels from the
+                    # top-k predictions some relevant items might not be included, but the
+                    # relevant count is necessary for many ranking metrics (e.g. recall, ndcg)
+                    (
+                        predictions_eval,
+                        targets_eval,
+                        relevant_count,
+                    ) = RankingMetric2.extract_topk_for_metrics(predictions, targets, max_k)
+
+            for metric in self.eval_metrics:
+                if isinstance(metric, RankingMetric2):
+                    metric_state = metric.update_state(
+                        targets_eval,
+                        predictions_eval,
+                        relevant_count,
+                        sample_weight=sample_weight,
+                    )
+                else:
+                    metric_state = metric.update_state(
+                        y_true=targets, y_pred=predictions, sample_weight=sample_weight
+                    )
+                update_ops.append(metric_state)
 
         for metric in self.prediction_metrics:
             update_ops.append(metric.update_state(predictions, sample_weight=sample_weight))
@@ -2314,7 +2344,7 @@ class Model(tf.keras.Model, LossMixin, MetricsMixin):
 
         # Initializing model control flags controled by MetricsComputeCallback()
         self._should_compute_train_metrics_for_batch = False
-        self._should_compute_eval_metrics_as_in_training = True
+        self._should_compute_eval_metrics_as_in_training = False
         self._is_first_batch = True
 
     def call(self, inputs, **kwargs):
@@ -2492,7 +2522,7 @@ class Model(tf.keras.Model, LossMixin, MetricsMixin):
             callbacks = []
 
         # Adding a callback to control metrics computation
-        callbacks = [MetricsComputeCallback(train_metrics_steps)] + callbacks
+        callbacks.append(MetricsComputeCallback(train_metrics_steps))
 
         return super().fit(
             x,
@@ -2514,43 +2544,6 @@ class Model(tf.keras.Model, LossMixin, MetricsMixin):
             max_queue_size,
             workers,
             use_multiprocessing,
-        )
-
-    def evaluate(
-        self,
-        x=None,
-        y=None,
-        batch_size=None,
-        verbose=1,
-        sample_weight=None,
-        steps=None,
-        callbacks=None,
-        max_queue_size=10,
-        workers=1,
-        use_multiprocessing=False,
-        return_dict=False,
-        **kwargs,
-    ):
-
-        if callbacks is None:
-            callbacks = []
-
-        # Adding a callback to control metrics computation
-        callbacks = [MetricsComputeCallback()] + callbacks
-
-        return super().evaluate(
-            x=x,
-            y=y,
-            batch_size=batch_size,
-            verbose=verbose,
-            sample_weight=sample_weight,
-            steps=steps,
-            callbacks=callbacks,
-            max_queue_size=max_queue_size,
-            workers=workers,
-            use_multiprocessing=use_multiprocessing,
-            return_dict=return_dict,
-            **kwargs,
         )
 
     def batch_predict(

merlin/models/tf/metrics/ranking.py
@@ -16,11 +16,17 @@
 
 # Adapted from source code: https://github.com/karlhigley/ranking-metrics-torch
 from abc import abstractmethod
-from typing import List, Sequence
+from typing import List, Optional, Sequence
 
 import numpy as np
 import tensorflow as tf
+from keras.utils import losses_utils, metrics_utils
+from keras.utils.tf_utils import is_tensor_or_variable
+from tensorflow.keras import backend
+from tensorflow.keras.metrics import Mean
+from tensorflow.keras.metrics import get as get_metric
 
+from ..utils.tf_utils import extract_topk
 from . import metrics_registry
 
 METRIC_PARAMETERS_DOCSTRING = """
@@ -343,5 +349,123 @@ def process_metrics(metrics, prefix=""):
     return metrics_proc
 
 
+@tf.keras.utils.register_keras_serializable(package="merlin.models")
+class RankingMetric2(Mean):
+    def __init__(self, fn, k=5, pre_sorted=True, name=None, dtype=None, **kwargs):
+        super().__init__(name=name, dtype=dtype)
+        self._fn = fn
+        self.k = k
+        self.pre_sorted = pre_sorted
+        self._fn_kwargs = kwargs
+
+    def update_state(
+        self,
+        y_true: tf.Tensor,
+        y_pred: tf.Tensor,
+        relevant_count: Optional[tf.Tensor] = None,
+        sample_weight: Optional[tf.Tensor] = None,
+    ):
+        y_true = tf.cast(y_true, self._dtype)
+        y_pred = tf.cast(y_pred, self._dtype)
+        [
+            y_true,
+            y_pred,
+        ], sample_weight = metrics_utils.ragged_assert_compatible_and_get_flat_values(
+            [y_true, y_pred], sample_weight
+        )
+        y_pred, y_true = losses_utils.squeeze_or_expand_dimensions(y_pred, y_true)
+
+        y_pred, y_true, relevant_count = self._maybe_sort_top_k(y_pred, y_true, relevant_count)
+
+        ag_fn = tf.__internal__.autograph.tf_convert(
+            self._fn, tf.__internal__.autograph.control_status_ctx()
+        )
+        matches = ag_fn(
+            y_true,
+            y_pred,
+            relevant_count=relevant_count,
+            k=self.k,
+            **self._fn_kwargs,
+        )
+        return super().update_state(matches, sample_weight=sample_weight)
+
+    def _maybe_sort_top_k(self, y_pred, y_true, relevant_count: tf.Tensor = None):
+        if not self.pre_sorted:
+            y_pred, y_true, relevant_count = RankingMetric.extract_topk_for_metrics(
+                y_pred, y_true, self.k
+            )
+        else:
+            if relevant_count is None:
+                raise Exception(
+                    "If y_true was pre-sorted (and truncated to top-k) you must "
+                    "provide relevant_count argument."
+                )
+            relevant_count = tf.cast(relevant_count, self._dtype)
+
+        return y_pred, y_true, relevant_count
+
+    @classmethod
+    def extract_topk_for_metrics(cls, y_pred, y_true, k):
+        # Computes the number of relevant items per row (before extracting only the top-k)
+        relevant_count = tf.reduce_sum(y_true, axis=-1)
+        y_pred, y_true = extract_topk(k, y_pred, y_true)
+        return y_pred, y_true, relevant_count
+
+    def get_config(self):
+        config = {}
+
+        if type(self) is RankingMetric2:
+            # Only include function argument when the object is a MeanMetricWrapper
+            # and not a subclass.
+            config["fn"] = self._fn
+            config["k"] = self.k
+            config["pre_sorted"] = self.pre_sorted
+
+            for k, v in self._fn_kwargs.items():
+                config[k] = backend.eval(v) if is_tensor_or_variable(v) else v
+            base_config = super(RankingMetric2, self).get_config()
+            return dict(list(base_config.items()) + list(config.items()))
+
+    @classmethod
+    def from_config(cls, config):
+        # Note that while MeanMetricWrapper itself isn't public, objects of this
+        # class may be created and added to the model by calling model.compile.
+        fn = config.pop("fn", None)
+        k = config.pop("k", None)
+        pre_sorted = config.pop("pre_sorted", None)
+        if cls is RankingMetric2:
+            return cls(get_metric(fn), k=k, pre_sorted=pre_sorted, **config)
+        return super(RankingMetric2, cls).from_config(config)
+
+
+# TODO: Ensure that y_true is complete when pre_sorted=True
+def recall_at(
+    y_true: tf.Tensor,
+    y_pred: tf.Tensor,
+    relevant_count: Optional[tf.Tensor],
+    k: int = 5,
+) -> tf.Tensor:
+    # Compute recalls at K
+    rel_indices = tf.where(relevant_count != 0)
+    rel_count = tf.gather_nd(relevant_count, rel_indices)
+
+    if tf.shape(rel_indices)[0] > 0:
+        rel_labels = tf.cast(tf.gather_nd(y_true, rel_indices)[:, : int(k)], backend.floatx())
+        results = tf.cast(
+            tf.math.divide(tf.reduce_sum(rel_labels, axis=-1), rel_count),
+            backend.floatx(),
+        )
+        return results
+    else:
+        return tf.convert_to_tensor([], dtype=backend.floatx())
+
+
+@metrics_registry.register_with_multiple_names("recall_at2", "recall2")
+class RecallAt2(RankingMetric2):
+    def __init__(self, k=5, pre_sorted=True, name="recall"):
+        super().__init__(recall_at, k=k, pre_sorted=pre_sorted, name=name)
+
+
 def ranking_metrics(top_ks: Sequence[int], **kwargs) -> Sequence[RankingMetric]:
-    return NDCGAt(top_ks, **kwargs), RecallAt(top_ks, **kwargs), AvgPrecisionAt(top_ks, **kwargs)
+    # return NDCGAt(top_ks, **kwargs), RecallAt(top_ks, **kwargs), AvgPrecisionAt(top_ks, **kwargs)
+    return [(RecallAt2(k)) for k in top_ks]

merlin/models/tf/prediction_tasks/retrieval.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from collections import Sequence as SequenceCollection
 from typing import List, Optional, Sequence
 
 import tensorflow as tf
@@ -27,7 +28,7 @@ from ..blocks.sampling.base import ItemSampler
 from ..blocks.sampling.in_batch import InBatchSampler
 from ..core import Block, MetricOrMetricClass
 from ..losses import LossType, loss_registry
-from ..metrics.ranking import ranking_metrics
+from ..metrics.ranking import RankingMetric2, ranking_metrics
 from .classification import MultiClassClassificationTask
 
 
@@ -94,7 +95,7 @@ class ItemRetrievalTask(MultiClassClassificationTask):
 
         super().__init__(
             loss=self.loss,
-            metrics=list(metrics),
+            metrics=metrics,
             target_name=target_name,
             task_name=task_name,
             task_block=task_block,
@@ -102,14 +103,23 @@ class ItemRetrievalTask(MultiClassClassificationTask):
             **kwargs,
         )
 
-        if self.metrics is not None and len(self.metrics) > 0:
-            ranking_metrics = list([metric for metric in metrics if hasattr(metric, "top_ks")])
-            if len(set(ranking_metrics)) > 0:
-                top_k = tf.reduce_max([metric.top_ks for metric in ranking_metrics]) - 1
+        if self.metrics is not None:
+            if isinstance(metrics, SequenceCollection):
+                metrics = list(metrics)
             else:
-                # Makes BruteForceTopK to score all items, without further sorting
-                top_k = None
-            self.pre_eval_topk = BruteForceTopK(k=top_k)
+                metrics = [metrics]
+
+            if len(self.metrics) > 0:
+                ranking_metrics = list(
+                    [metric for metric in metrics if isinstance(metric, RankingMetric2)]
+                )
+
+                if len(ranking_metrics) > 0:
+                    top_k = tf.reduce_max([metric.k for metric in ranking_metrics]) - 1
+                else:
+                    # Makes BruteForceTopK to score all items, without further sorting
+                    top_k = None
+                self.pre_eval_topk = BruteForceTopK(k=top_k)
 
     def _build_prediction_call(
         self,