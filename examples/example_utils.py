892f5bdb5a55b380f1a8805363ad73c94c59b028
root <ronayak@hotmail.com>
2022-03-17 16:57:25 +0000

    update

--- examples/example_utils.py
+++ examples/example_utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import nvtabular as nvt
 
 
-def workflow_fit_transform(outputs, train_path, test_path, output_path):
+def workflow_fit_transform(outputs, train_path, test_path, output_path, workflow_name=None):
     workflow = nvt.Workflow(outputs)
 
     train_dataset = nvt.Dataset(train_path)
@@ -14,6 +14,13 @@ def workflow_fit_transform(outputs, train_path, test_path, output_path):
     workflow.transform(train_dataset).to_parquet(output_path=output_path + "/train/")
 
     workflow.transform(test_dataset).to_parquet(output_path=output_path + "/test/")
+
+    if workflow_name is None:
+        workflow_name = "workflow"
+    else:
+        workflow_name = workflow_name
+    # save workflow to the pwd
+    workflow.save(workflow_name)
 
 
 def save_results(model_name, model):
@@ -36,11 +43,8 @@ def create_bar_chart(text_file_name, models_name):
 
     X_axis = np.arange(len(models_name))
 
-    cmap = plt.cm.tab10
-    colors = cmap(np.arange(len(models_name)) % cmap.N)
-
     plt.title("Models' accuracy metrics comparison", pad=20)
-    plt.bar(X_axis - 0.2, auc, 0.4, label="AUC", color=colors)
+    plt.bar(X_axis - 0.2, auc, 0.4, label="AUC")
 
     plt.xticks(X_axis, models_name)
     plt.xlabel("Models")
