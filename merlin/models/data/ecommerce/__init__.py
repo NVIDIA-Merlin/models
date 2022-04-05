3ea5d88e30efc3519e44e7c74599dc515acbe9de
Marc Romeyn <marcromeyn@gmail.com>
2022-04-05 09:46:17 +0200

    Adding init files for various datasets

--- merlin/models/data/ecommerce/__init__.py
+++ merlin/models/data/ecommerce/__init__.py
@@ -13,3 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+from merlin.models.data.ecommerce.aliccp.dataset import (
+    default_aliccp_transformation,
+    get_aliccp,
+    prepare_alliccp,
+    transform_aliccp,
+)
+
+__all__ = ["prepare_alliccp", "transform_aliccp", "get_aliccp", "default_aliccp_transformation"]
--- merlin/models/data/entertainment/__init__.py
+++ merlin/models/data/entertainment/__init__.py
@@ -13,3 +13,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from merlin.models.data.entertainment.movielens.dataset import (
+    default_ml1m_transformation,
+    default_ml25m_transformation,
+    default_ml100k_transformation,
+    get_movielens,
+    transform_movielens,
+)
+
+__all__ = [
+    "get_movielens",
+    "transform_movielens",
+    "default_ml100k_transformation",
+    "default_ml1m_transformation",
+    "default_ml25m_transformation",
+]
