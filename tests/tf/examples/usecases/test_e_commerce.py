ff0cbce0c706e5aefe4dc797b0bd11f949156c59
Marc Romeyn <marcromeyn@gmail.com>
2022-04-07 15:29:38 +0200

    Move pytest marker decorators to the top

--- tests/tf/examples/usecases/test_e_commerce.py
+++ tests/tf/examples/usecases/test_e_commerce.py
@@ -2,8 +2,8 @@ import pytest
 from testbook import testbook
 
 
-@testbook("examples/usecases/e-commerce.ipynb", execute=True)
 @pytest.mark.example
+@testbook("examples/usecases/e-commerce.ipynb", execute=True)
 def test_tf_example_ecommerce_usecase(tb):
     assert tb.cell_output_text(2)[-19:] == "parameters: 1112962"
     assert tb.cell_output_text(4)[-18:] == "parameters: 393444"
--- tests/tf/examples/usecases/test_music_streaming.py
+++ tests/tf/examples/usecases/test_music_streaming.py
@@ -2,8 +2,8 @@ import pytest
 from testbook import testbook
 
 
-@testbook("examples/usecases/music-streaming.ipynb", execute=True)
 @pytest.mark.example
+@testbook("examples/usecases/music-streaming.ipynb", execute=True)
 def test_tf_example_music_streaming_usecase(tb):
     assert tb.cell_output_text(2)[-19:] == "parameters: 1094466"
     assert tb.cell_output_text(4)[-19:] == "parameters: 2006533"
