04aeddb1758f73c933403038b6e71860fde6b721
Benedikt Schifferer <bschifferer@nvidia.com>
2022-04-12 12:09:14 +0000

    add export notebooks

---
+++ tests/tf/examples/test_04_export_ranking_models.py
@@ -0,0 +1,18 @@
+import pytest
+import os
+
+from testbook import testbook
+
+pytestmark = pytest.mark.example
+
+@testbook("examples/04-Exporting-ranking-models.ipynb", execute=False)
+def test_func(tb):
+    tb.inject(
+        """
+        import os
+        os.environ["DATA_FOLDER"] = "/tmp/data/"
+        """
+    )
+    tb.execute()
+    assert os.path.isdir('dlrm')
+    assert os.path.isdir('workflow')
---
+++ tests/tf/examples/test_05_export_retrieval_model.py
@@ -0,0 +1,20 @@
+import pytest
+import os
+
+from testbook import testbook
+
+pytestmark = pytest.mark.example
+
+@testbook("examples/05-Retrieval-Model.ipynb", execute=False)
+def test_func(tb):
+    tb.inject(
+        """
+        import os
+        os.environ["DATA_FOLDER"] = "/tmp/data/"
+        """
+    )
+    tb.execute()
+    assert os.path.isdir('query_tower')
+    assert os.path.exists('user_features.parquet')
+    assert os.path.exists('item_embeddings.parquet')
+    assert os.path.exists('item_features.parquet')
