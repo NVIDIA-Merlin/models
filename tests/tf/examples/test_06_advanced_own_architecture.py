307712be45ed056faf0621e0b03de35b928e4033
Benedikt Schifferer <bschifferer@nvidia.com>
2022-04-12 12:16:19 +0000

    advanced example

---
+++ tests/tf/examples/test_06_advanced_own_architecture.py
@@ -0,0 +1,34 @@
+import pytest
+import os
+
+from testbook import testbook
+
+pytestmark = pytest.mark.example
+
+@testbook("examples/06-Define-your-own-architecture-with-Merlin-Models.ipynb", execute=False)
+def test_func(tb):
+    tb.inject(
+        """
+        from unittest.mock import patch
+        from merlin.datasets.synthetic import generate_data
+        mock_train, mock_valid = generate_data(
+            input="movielens-1m",
+            num_rows=1000,
+            set_sizes=(0.8, 0.2)
+        )
+        p1 = patch(
+            "merlin.datasets.entertainment.get_movielens",
+            return_value=[mock_train, mock_valid]
+        )
+        p1.start()
+        """
+    )
+    tb.execute()
+    metrics = tb.ref("metrics")
+    assert sorted(list(metrics.keys())) == [
+        "loss",
+        "rating_binary/binary_classification_task/auc",
+        "regularization_loss",
+        "total_loss"
+    ]
+    assert os.path.isdir('custom_dlrm')
