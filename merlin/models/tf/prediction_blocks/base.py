cf1c7a0755521f2880bc56d1372edf948d467085
Marc Romeyn <marcromeyn@gmail.com>
2022-04-14 10:50:27 +0200

    Trying to make metrics more dynamic

--- merlin/models/tf/prediction_blocks/base.py
+++ merlin/models/tf/prediction_blocks/base.py
@@ -8,11 +8,24 @@ if TYPE_CHECKING:
     from merlin.models.tf.models.base import Model
 
 
+class PreLossBlock(Block):
+    def __init__(self, model: Optional[Model] = None, **kwargs):
+        super().__init__(**kwargs)
+        if model:
+            self.model = model
+
+    # OR pre_loss
+    def pre_loss(self, inputs: PredictionOutput, training=False, **kwargs) -> PredictionOutput:
+        return self.model.pre_loss()
+
+
+# Or PreLossBlock
 class PredictionBlock(Block):
     def __init__(self, model: Optional[Model] = None, **kwargs):
         super().__init__(**kwargs)
         if model:
             self.model = model
 
+    # OR pre_loss
     def call(self, inputs: PredictionOutput, training=False, **kwargs) -> PredictionOutput:
         return inputs
