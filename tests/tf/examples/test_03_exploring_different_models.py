4adb7ca0565b9c9d71b6c6ee9ae7cfbb1d9871ba
root <ronayak@hotmail.com>
2022-04-28 15:54:24 +0000

    add unit test for 03_exploring_different_models

---
+++ tests/tf/examples/test_03_exploring_different_models.py
@@ -0,0 +1,46 @@
+import os
+
+from testbook import testbook
+
+from tests.conftest import REPO_ROOT
+
+
+@testbook(REPO_ROOT / "examples/03-Exploring-different-models.ipynb", execute=False)
+def test_func(tb):
+    tb.inject(
+        """
+        import os
+        os.environ["DATA_FOLDER"] = "/tmp/data/"
+        os.environ["NUM_ROWS"] = "999"
+        """
+    )
+    tb.execute()
+    metrics_ncf = tb.ref("metrics_ncf")
+    assert sorted(list(metrics_ncf.keys())) == [
+        "auc",
+        "loss",
+        "regularization_loss",
+        "total_loss",
+    ]
+    metrics_mlp = tb.ref("metrics_mlp")
+    assert sorted(list(metrics_mlp.keys())) == [
+        "auc_1",
+        "loss",
+        "regularization_loss",
+        "total_loss",
+    ]
+    metrics_dlrm = tb.ref("metrics_dlrm")
+    assert sorted(list(metrics_dlrm.keys())) == [
+        "auc_2",
+        "loss",
+        "regularization_loss",
+        "total_loss",
+    ]
+    metrics_dcn = tb.ref("metrics_dcn")
+    assert sorted(list(metrics_dcn.keys())) == [
+        "auc_3",
+        "loss",
+        "regularization_loss",
+        "total_loss",
+    ]
+    assert os.path.isfile("results.txt")
