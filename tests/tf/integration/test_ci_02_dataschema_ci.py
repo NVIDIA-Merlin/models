8f6902dd046d48dc151eca2f7ff329bef64de836
bschifferer <bschifferer@nvidia.com>
2022-05-05 09:21:01 +0200

    ci movielens notebooks (#411)

---
+++ tests/tf/integration/test_ci_02_dataschema_ci.py
@@ -0,0 +1,27 @@
+import pytest
+from testbook import testbook
+
+from tests.conftest import REPO_ROOT
+
+
+@pytest.mark.ci
+@testbook(REPO_ROOT / "examples/02-Merlin-Models-and-NVTabular-integration.ipynb", execute=False)
+def test_func(tb):
+    tb.inject(
+        """
+        import os
+        os.environ["INPUT_DATA_DIR"] = "/raid/data/movielens"
+        """
+    )
+    tb.execute()
+    assert tb.cell_output_text(15)[-19:] == "'TE_userId_rating']"
+    metrics = tb.ref("metrics")
+    assert sorted(list(metrics.keys())) == [
+        "loss",
+        "rating_binary/binary_classification_task/auc",
+        "rating_binary/binary_classification_task/binary_accuracy",
+        "rating_binary/binary_classification_task/precision",
+        "rating_binary/binary_classification_task/recall",
+        "regularization_loss",
+        "total_loss",
+    ]
