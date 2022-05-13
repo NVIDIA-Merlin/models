bdeac4e2d721c9d1e30fee6fcc4f93ff55a42eed
Marc Romeyn <marcromeyn@gmail.com>
2022-05-13 17:35:49 +0200

    Some commit

--- tests/unit/tf/utils/test_batch.py
+++ tests/unit/tf/utils/test_batch.py
@@ -7,14 +7,12 @@ from merlin.io import Dataset
 
 @pytest.mark.parametrize("run_eagerly", [True, False])
 def test_model_encode(ecommerce_data: Dataset, run_eagerly):
-    prediction_task = ml.PredictionTasks(ecommerce_data.schema)
-
     body = ml.InputBlock(ecommerce_data.schema).connect(ml.MLPBlock([64]))
+    prediction_task = ml.PredictionTasks(ecommerce_data.schema)
     model = ml.Model(body, prediction_task)
+
     model.compile(run_eagerly=run_eagerly, optimizer="adam")
-
     model.fit(ecommerce_data, batch_size=50, epochs=1)
-
     data = model.batch_predict(ecommerce_data, batch_size=10)
     ddf = data.compute(scheduler="synchronous")
 
@@ -26,7 +24,7 @@ def test_two_tower_embedding_extraction(ecommerce_data: Dataset):
     two_tower = ml.TwoTowerBlock(ecommerce_data.schema, query_tower=ml.MLPBlock([64, 128]))
 
     model = ml.RetrievalModel(
-        two_tower, ml.ItemRetrievalTask(ecommerce_data.schema, target_name="click", metrics=[])
+        two_tower, ml.ItemRetrievalTask(ecommerce_data.schema, target_name="click")
     )
     model.compile(run_eagerly=True, optimizer="adam")
     model.fit(ecommerce_data, batch_size=50, epochs=1)
@@ -48,7 +46,7 @@ def test_two_tower_extracted_embeddings_are_equal(ecommerce_data: Dataset):
     two_tower = ml.TwoTowerBlock(ecommerce_data.schema, query_tower=ml.MLPBlock([64, 128]))
 
     model = ml.RetrievalModel(
-        two_tower, ml.ItemRetrievalTask(ecommerce_data.schema, target_name="click", metrics=[])
+        two_tower, ml.ItemRetrievalTask(ecommerce_data.schema, target_name="click")
     )
     model.compile(run_eagerly=True, optimizer="adam")
     model.fit(ecommerce_data, batch_size=50, epochs=1)
