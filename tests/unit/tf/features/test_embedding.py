c41ae8f68291f5c159622c0c840841ab3f7156ad
Gabriel Moreira <gmoreira@nvidia.com>
2022-05-18 15:00:29 -0300

    Fixing the issue when exporting embeddings to cudf DataFrame

--- tests/unit/tf/features/test_embedding.py
+++ tests/unit/tf/features/test_embedding.py
@@ -275,12 +275,6 @@ def test_embedding_features_yoochoose_pretrained_initializer(testing_data: Datas
     assert np.allclose(emb_module.embedding_tables["categories"].numpy(), pretrained_emb_categories)
 
 
-@pytest.mark.skip(
-    reason="This test fails because cudf.from_dlpack() we use internally in "
-    "EmbeddingFeatures().embedding_table_dataset() does not work properly. "
-    "This test should be enabled when the following cudf issue we reported is resolved: "
-    "https://github.com/rapidsai/cudf/issues/10754"
-)
 def test_embedding_features_exporting_and_loading_pretrained_initializer(testing_data: Dataset):
     schema = testing_data.schema.select_by_tag(Tags.CATEGORICAL)
     emb_module = mm.EmbeddingFeatures.from_schema(schema)
