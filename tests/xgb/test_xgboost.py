1ae3e5ad43d6fe91e497460f821a8d6883d5a2c2
Oliver Holworthy <oliver@holworthy.com>
2022-05-19 15:07:22 +0100

    Use objective as argument to constructor

--- tests/xgb/test_xgboost.py
+++ tests/xgb/test_xgboost.py
@@ -19,9 +19,8 @@ from merlin.io import Dataset
 from merlin.models.xgb import XGBoost
 
 
-def test_xgboost(music_streaming_data: Dataset):
-    objective = "reg:logistic"
-    model = XGBoost(objective)
+def test_music_regression(music_streaming_data: Dataset):
+    model = XGBoost(objective="reg:logistic")
     model.fit(music_streaming_data)
     model.predict(music_streaming_data)
     metrics = model.evaluate(music_streaming_data)
@@ -30,8 +29,34 @@ def test_xgboost(music_streaming_data: Dataset):
 
 
 def test_unsupported_objective(music_streaming_data: Dataset):
-    objective = "reg:unknown"
     with pytest.raises(ValueError) as excinfo:
-        model = XGBoost(objective)
+        model = XGBoost(objective="reg:unknown")
         model.fit(music_streaming_data)
     assert "Objective not supported" in str(excinfo.value)
+
+
+def test_ecommerce_click(ecommerce_data: Dataset):
+    model = XGBoost(objective="binary:logistic", eval_metric="auc")
+    model.fit(ecommerce_data, target_columns=["click"])
+    model.predict(ecommerce_data)
+    metrics = model.evaluate(ecommerce_data)
+
+    assert "auc" in metrics
+
+
+def test_social_click(social_data: Dataset):
+    model = XGBoost(objective="binary:logistic", eval_metric=["auc"])
+    model.fit(social_data, target_columns=["click"])
+    model.predict(social_data)
+    metrics = model.evaluate(social_data)
+
+    assert "auc" in metrics
+
+
+def test_criteo(criteo_data: Dataset):
+    model = XGBoost(objective="binary:logistic", eval_metric=["auc"])
+    model.fit(criteo_data)
+    model.predict(criteo_data)
+    metrics = model.evaluate(criteo_data)
+
+    assert "auc" in metrics
