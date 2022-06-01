1a875368212e1448ba51e79896e8153cdcb8f6fa
Oliver Holworthy <oliver@holworthy.com>
2022-06-01 16:23:27 +0100

    Add test for calling filter block

--- tests/unit/tf/blocks/core/test_tabular.py
+++ tests/unit/tf/blocks/core/test_tabular.py
@@ -1,28 +1,43 @@
-import merlin.models.tf as ml
+import merlin.models.tf as mm
+from merlin.schema import Schema, Tags
 
 
 def test_filter_features(tf_con_features):
     features = ["a", "b"]
-    con = ml.Filter(features)(tf_con_features)
+    con = mm.Filter(features)(tf_con_features)
 
     assert list(con.keys()) == features
 
 
+def test_filter_call(tf_con_features):
+    assert mm.Filter([])({}) == {}
+    assert mm.Filter([])(tf_con_features) == {}
+    assert mm.Filter(["unknown"])(tf_con_features) == {}
+
+    assert mm.Filter(Schema())({}) == {}
+    assert mm.Filter(Schema())(tf_con_features) == {}
+    assert mm.Filter(Schema(["unknown"]))(tf_con_features) == {}
+
+    assert mm.Filter(Tags.CONTINUOUS)({}) == {}
+    # TODO : Raises TypeError
+    # assert mm.Filter(Tags.CONTINUOUS)(tf_con_features) == {}
+
+
 def test_as_tabular(tf_con_features):
     name = "tabular"
-    con = ml.AsTabular(name)(tf_con_features)
+    con = mm.AsTabular(name)(tf_con_features)
 
     assert list(con.keys()) == [name]
 
 
 def test_tabular_block(tf_con_features):
-    _DummyTabular = ml.TabularBlock
+    _DummyTabular = mm.TabularBlock
 
     tabular = _DummyTabular()
 
     assert tabular(tf_con_features) == tf_con_features
     assert tabular(tf_con_features, aggregation="concat").shape[1] == 6
-    assert tabular(tf_con_features, aggregation=ml.ConcatFeatures()).shape[1] == 6
+    assert tabular(tf_con_features, aggregation=mm.ConcatFeatures()).shape[1] == 6
 
     tabular_concat = _DummyTabular(aggregation="concat")
     assert tabular_concat(tf_con_features).shape[1] == 6
