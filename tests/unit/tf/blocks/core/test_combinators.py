435d89c172a23b8443b5c0d5ad6bbde58e6d24a9
Marc Romeyn <marcromeyn@gmail.com>
2022-06-01 12:51:35 +0200

    Adding serialization test for ParallelBlock

--- tests/unit/tf/blocks/core/test_combinators.py
+++ tests/unit/tf/blocks/core/test_combinators.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 import merlin.models.tf as mm
@@ -27,3 +28,21 @@ def test_parallel_block_pruning(music_streaming_data: Dataset, name_branches: bo
 
     assert len(outputs) == 7  # There are 7 categorical features
     assert continuous_block not in input_block.parallel_values
+
+
+def test_parallel_block_serialization(music_streaming_data: Dataset):
+    unkown_filter = mm.Filter(["none"])
+    block = mm.ParallelBlock(mm.Filter(["position"]), unkown_filter, automatic_pruning=False)
+    block_copy = block.from_config(block.get_config())
+
+    assert not block_copy.automatic_pruning
+    assert unkown_filter not in block_copy.parallel_values
+
+    features = mm.sample_batch(music_streaming_data, batch_size=10, include_targets=False)
+
+    outputs_1 = block(features)
+    outputs_2 = block_copy(features)
+
+    for key in outputs_1:
+        np.testing.assert_array_equal(outputs_2[key].numpy(), outputs_1[key].numpy())
+    assert len(outputs_1) == len(outputs_2) == 1
