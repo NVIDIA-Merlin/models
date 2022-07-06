9f3eba9fbc5c9cecdceee8ec57b1b5314e44305f
Oliver Holworthy <oliver@holworthy.com>
2022-07-06 14:16:00 +0100

    Add Cond to merlin.models.tf export

--- merlin/models/tf/__init__.py
+++ merlin/models/tf/__init__.py
@@ -42,7 +42,7 @@ from merlin.models.tf.blocks.core.base import (
     NoOp,
     right_shift_layer,
 )
-from merlin.models.tf.blocks.core.combinators import ParallelBlock, ResidualBlock, SequentialBlock
+from merlin.models.tf.blocks.core.combinators import ParallelBlock, ResidualBlock, SequentialBlock, Cond
 from merlin.models.tf.blocks.core.context import FeatureContext
 from merlin.models.tf.blocks.core.index import IndexBlock, TopKIndexBlock
 from merlin.models.tf.blocks.core.masking import CausalLanguageModeling, MaskedLanguageModeling
@@ -126,6 +126,7 @@ OptimizerV2.__repr__ = repr_utils.layer_repr_no_children
 
 __all__ = [
     "Block",
+    "Cond",
     "ModelContext",
     "SequentialBlock",
     "ResidualBlock",
