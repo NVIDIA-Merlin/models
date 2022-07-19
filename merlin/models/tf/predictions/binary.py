75135dec5c234e7f347c36bd9ef062af21542461
Marc Romeyn <marcromeyn@gmail.com>
2022-07-19 11:28:42 +0200

    Running black

--- merlin/models/tf/predictions/binary.py
+++ merlin/models/tf/predictions/binary.py
@@ -4,19 +4,21 @@ from merlin.models.tf.predictions.base import PredictionBlock
 
 
 class BinaryPrediction(PredictionBlock):
+    """Binary-classification prediction block"""
+
     def __init__(
-            self,
-            default_loss="binary_crossentropy",
-            default_metrics=(
-                    tf.keras.metrics.Precision,
-                    tf.keras.metrics.Recall,
-                    tf.keras.metrics.BinaryAccuracy,
-                    tf.keras.metrics.AUC,
-            ),
-            target=None,
-            pre=None,
-            post=None,
-            logits_temperature=1.0
+        self,
+        default_loss="binary_crossentropy",
+        default_metrics=(
+            tf.keras.metrics.Precision,
+            tf.keras.metrics.Recall,
+            tf.keras.metrics.BinaryAccuracy,
+            tf.keras.metrics.AUC,
+        ),
+        target=None,
+        pre=None,
+        post=None,
+        logits_temperature=1.0,
     ):
         super().__init__(
             prediction=tf.keras.layers.Dense(1, activation="sigmoid"),
@@ -25,5 +27,5 @@ class BinaryPrediction(PredictionBlock):
             target=target,
             pre=pre,
             post=post,
-            logits_temperature=logits_temperature
+            logits_temperature=logits_temperature,
         )
