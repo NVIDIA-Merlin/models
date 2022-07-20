ebc75b32babb05b2d67e36bec1030b20ca7acb36
Marc Romeyn <marcromeyn@gmail.com>
2022-07-20 14:37:52 +0200

    Restore slower test_train_metrics_steps since new version is flaky

--- tests/unit/tf/models/test_base.py
+++ tests/unit/tf/models/test_base.py
@@ -99,8 +99,8 @@ class MetricsLogger(tf.keras.callbacks.Callback):
     ["num_rows", "batch_size", "train_metrics_steps", "expected_steps", "expected_metrics_steps"],
     [
         (1, 1, 1, 1, 1),
-        # (60, 10, 2, 6, 3),
-        # (60, 10, 3, 6, 2),
+        (60, 10, 2, 6, 3),
+        (60, 10, 3, 6, 2),
         (120, 10, 4, 12, 3),
     ],
 )
@@ -108,10 +108,9 @@ def test_train_metrics_steps(
     num_rows, batch_size, train_metrics_steps, expected_steps, expected_metrics_steps
 ):
     dataset = generate_data("e-commerce", num_rows=num_rows)
-    dataset.schema = dataset.schema.select_by_name(["user_id", "click"])
     model = ml.Model(
         ml.InputBlock(dataset.schema),
-        ml.MLPBlock([2]),
+        ml.MLPBlock([64]),
         ml.BinaryClassificationTask("click"),
     )
     model.compile(
