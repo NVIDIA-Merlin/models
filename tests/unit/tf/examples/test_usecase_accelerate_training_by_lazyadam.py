eaaa1756ab11491ad92504e474152b026384b712
rnyak <ronayak@hotmail.com>
2022-09-28 17:56:27 -0400

    fix pre-commit issue

--- tests/unit/tf/examples/test_usecase_accelerate_training_by_lazyadam.py
+++ tests/unit/tf/examples/test_usecase_accelerate_training_by_lazyadam.py
@@ -23,8 +23,24 @@ def test_usecase_accelerate_training_by_lazyadam(tb):
     model1_lazyadam = tb.ref("model1")
     model2_adam = tb.ref("model2")
     assert set(model1_lazyadam.history.history.keys()) == set(
-        ['loss', 'recall_at_10', 'mrr_at_10', 'ndcg_at_10', 'map_at_10', 'precision_at_10', 'regularization_loss']
+        [
+            "loss",
+            "recall_at_10",
+            "mrr_at_10",
+            "ndcg_at_10",
+            "map_at_10",
+            "precision_at_10",
+            "regularization_loss",
+        ]
     )
     assert set(model2_adam.history.history.keys()) == set(
-        ['loss', 'recall_at_10', 'mrr_at_10', 'ndcg_at_10', 'map_at_10', 'precision_at_10', 'regularization_loss']
+        [
+            "loss",
+            "recall_at_10",
+            "mrr_at_10",
+            "ndcg_at_10",
+            "map_at_10",
+            "precision_at_10",
+            "regularization_loss",
+        ]
     )
