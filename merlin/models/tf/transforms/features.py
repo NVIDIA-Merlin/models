fe0239fce6a07edeef1d75416bf603bdd9e03fec
edknv <edwardk@nvidia.com>
2022-10-09 17:42:58 -0700

    Change output format of ToTarget to tuple

--- merlin/models/tf/transforms/features.py
+++ merlin/models/tf/transforms/features.py
@@ -741,10 +741,7 @@ class ToTarget(Block):
         self, inputs: TabularData, targets=None, testing=False, **kwargs
     ) -> "PredictionOutput":
         if testing:
-            return PredictionOutput(predictions=inputs, targets=targets)
-
-        if targets is None:
-            targets = {}
+            return inputs, targets
 
         target_columns = self._target_column_schemas()
 
@@ -758,7 +755,7 @@ class ToTarget(Block):
             else:
                 targets = inputs[name]
 
-        return PredictionOutput(predictions=outputs, targets=targets or None)
+        return outputs, targets
 
     def compute_output_shape(self, input_shape):
         return input_shape
