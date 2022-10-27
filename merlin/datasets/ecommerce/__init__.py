eeab374db61caa14e8282fa07d881ac60e17ce93
Marc Romeyn <marcromeyn@gmail.com>
2022-10-27 13:57:03 +0200

    Triggering black

--- merlin/datasets/ecommerce/__init__.py
+++ merlin/datasets/ecommerce/__init__.py
@@ -20,8 +20,8 @@ from merlin.datasets.ecommerce.aliccp.dataset import (
     prepare_alliccp,
     transform_aliccp,
 )
-from merlin.datasets.ecommerce.dressipi.dataset import get_dressipi2022
 from merlin.datasets.ecommerce.booking.dataset import get_booking, transform_booking
+from merlin.datasets.ecommerce.dressipi.dataset import get_dressipi2022
 
 __all__ = [
     "prepare_alliccp",
@@ -30,5 +30,5 @@ __all__ = [
     "default_aliccp_transformation",
     "get_dressipi2022",
     "get_booking",
-    "transform_booking"
+    "transform_booking",
 ]
