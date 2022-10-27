4f25bceaf2e8fc343d6ade35852b3082386891d1
Oliver Holworthy <oholworthy@nvidia.com>
2022-10-27 13:25:22 +0100

    Update test for transformer retrieval model

--- tests/unit/tf/transformers/test_block.py
+++ tests/unit/tf/transformers/test_block.py
@@ -31,39 +31,51 @@ def test_retrieval_transformer(sequence_testing_data: Dataset, run_eagerly):
     seq_schema = sequence_testing_data.schema.select_by_tag(Tags.SEQUENCE).select_by_tag(
         Tags.CATEGORICAL
     )
-    target = sequence_testing_data.schema.select_by_tag(Tags.ITEM_ID).column_names[0]
 
-    loader = Loader(sequence_testing_data, batch_size=8, shuffle=False)
+    target = sequence_testing_data.schema.select_by_tag(Tags.ITEM_ID).column_names[0]
+    predict_next = mm.SequencePredictNext(schema=seq_schema, target=target)
+    loader = Loader(sequence_testing_data, batch_size=8, shuffle=False, transform=predict_next)
+
+    query_schema = seq_schema
+    output_schema = seq_schema.select_by_name(target)
+
+    query_encoder = mm.Encoder(
+        mm.InputBlockV2(
+            query_schema,
+            embeddings=mm.Embeddings(
+                query_schema.select_by_tag(Tags.CATEGORICAL), sequence_combiner=None
+            ),
+        ),
+        GPT2Block(d_model=48, n_head=4, n_layer=2, pre=mm.ReplaceMaskedEmbeddings()),
+        tf.keras.layers.Lambda(lambda x: tf.reduce_mean(x, axis=1)),
+    )
 
     model = mm.RetrievalModelV2(
-        query=mm.SequentialBlock(
-            [
-                mm.InputBlockV2(
-                    seq_schema,
-                    embeddings=mm.Embeddings(
-                        seq_schema.select_by_tag(Tags.CATEGORICAL), sequence_combiner=None
-                    ),
-                ),
-                GPT2Block(d_model=48, n_head=4, n_layer=2, pre=mm.ReplaceMaskedEmbeddings()),
-            ]
-        ),
-        output=mm.ContrastiveOutput(
-            seq_schema.select_by_name(target), negative_samplers="in-batch"
-        ),
+        query=query_encoder,
+        output=mm.ContrastiveOutput(output_schema, negative_samplers="in-batch"),
     )
-    seq_mask_random = mm.SequenceMaskRandom(schema=seq_schema, target=target, masking_prob=0.3)
 
-    inputs, targets = next(iter(loader))
-    outputs = model(inputs, targets=targets, training=True)
-    assert list(outputs.shape) == [8, 4, 51997]
     testing_utils.model_test(
         model,
         loader,
         run_eagerly=run_eagerly,
-        reload_model=True,
-        fit_kwargs={"pre": seq_mask_random},
-        metrics=[mm.RecallAt(5000), mm.NDCGAt(5000)],
+        reload_model=False,
+        metrics={},
     )
+
+    predictions = model.predict(loader)
+    assert list(predictions.shape) == [100, 51997]
+
+    query_embeddings = query_encoder.predict(loader)
+    assert list(query_embeddings.shape) == [100, 48]
+
+    # query_embeddings = model.query_embeddings(sequence_testing_data, batch_size=10).compute()
+    item_embeddings = model.candidate_embeddings().compute().to_numpy()
+
+    assert list(item_embeddings.shape) == [51997, 48]
+    predicitons_2 = np.dot(query_embeddings, item_embeddings.T)
+
+    np.testing.assert_allclose(predictions, predicitons_2, atol=1e-7)
 
 
 def test_retrieval(sequence_testing_data: Dataset):
