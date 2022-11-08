2dbf502e4b683f4cf9c53e2f2e5bffed3ce82fbc
Radek Osmulski <rosmulski@gmail.com>
2022-11-08 16:45:39 +1000

    fix unit test; clean up

--- tests/unit/tf/examples/test_usecase_transformers_next_item_prediction.py
+++ tests/unit/tf/examples/test_usecase_transformers_next_item_prediction.py
@@ -11,8 +11,7 @@ def test_usecase_pretrained_embeddings(tb):
         from datetime import datetime, timedelta
         from merlin.datasets.synthetic import generate_data
         ds = generate_data('booking.com-raw', 10000)
-        if not os.path.exists('~/merlin-models-data/booking'):
-            os.makedirs('~/merlin-models-data/booking')
+        if not os.path.exists('./data'): os.mkdir('./data')
         df = ds.compute()
         def generate_date():
             date = datetime.today()
@@ -20,12 +19,12 @@ def test_usecase_pretrained_embeddings(tb):
                 date -= timedelta(days=7)
             return date
         df['checkin'] = [generate_date() for _ in range(df.shape[0])]
-        df.to_csv('~/merlin-models-data/booking/train_set.csv')
+        df.to_csv('./data/train_set.csv')
         """
     )
     # import pdb
     # pdb.set_trace()
-    tb.cells[4].source = tb.cells[4].source.replace("get_booking(None)", "")
-    tb.cells[30].source = tb.cells[30].source.replace("d_model=40", "d_model=16")
-    tb.cells[32].source = tb.cells[32].source.replace("epochs=5", "epochs=1")
+    tb.cells[4].source = tb.cells[4].source.replace("get_booking('./data')", "")
+    tb.cells[29].source = tb.cells[29].source.replace("d_model=40", "d_model=16")
+    tb.cells[31].source = tb.cells[31].source.replace("epochs=5", "epochs=1")
     tb.execute()
