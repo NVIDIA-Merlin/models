83863dce3facef404f42cc0e067dfed27fb5c2f3
Radek Osmulski <rosmulski@gmail.com>
2022-11-09 13:59:40 +1000

    update

--- tests/unit/tf/examples/test_usecase_transformers_next_item_prediction.py
+++ tests/unit/tf/examples/test_usecase_transformers_next_item_prediction.py
@@ -19,12 +19,13 @@ def test_usecase_pretrained_embeddings(tb):
                 date -= timedelta(days=7)
             return date
         df['checkin'] = [generate_date() for _ in range(df.shape[0])]
+        df['checkout'] = [generate_date() for _ in range(df.shape[0])]
         df.to_csv('./data/train_set.csv')
         """
     )
     # import pdb
     # pdb.set_trace()
     tb.cells[4].source = tb.cells[4].source.replace("get_booking('./data')", "")
-    tb.cells[29].source = tb.cells[29].source.replace("d_model=40", "d_model=16")
-    tb.cells[31].source = tb.cells[31].source.replace("epochs=5", "epochs=1")
+    tb.cells[28].source = tb.cells[28].source.replace("d_model=64", "d_model=40")
+    tb.cells[30].source = tb.cells[30].source.replace("epochs=5", "epochs=1")
     tb.execute()
