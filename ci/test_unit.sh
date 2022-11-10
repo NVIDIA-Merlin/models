06346222da0ddcc1e40124b7f1a3bd5b68262d4d
edknv <edwardk@nvidia.com>
2022-11-10 07:20:12 -0800

    Update tox.ini to not use sitepackages

--- ci/test_unit.sh
+++ ci/test_unit.sh
@@ -17,9 +17,9 @@
 #!/bin/bash
 set -e
 
-tox -e py38-gpu $@
+tox -r -e py38-gpu $@
 
 # skip multi-gpu testing if any arguments are supplied.
 if [[ $# -eq 0 ]] ; then
-  tox -e py38-multi-gpu
+  tox -r -e py38-multi-gpu
 fi
