c382f37112a37c77a5fd80a761a66dbd7eabe86c
Radek Osmulski <rosmulski@gmail.com>
2022-11-10 19:22:42 +1000

    use absolute path for data

--- tests/unit/tf/examples/test_usecase_transformers_next_item_prediction.py
+++ tests/unit/tf/examples/test_usecase_transformers_next_item_prediction.py
@@ -20,12 +20,10 @@ def test_usecase_pretrained_embeddings(tb):
             return date
         df['checkin'] = [generate_date() for _ in range(df.shape[0])]
         df['checkout'] = [generate_date() for _ in range(df.shape[0])]
-        df.to_csv('./data/train_set.csv')
+        df.to_csv('/workspace/data/train_set.csv')
         """
     )
-    # import pdb
-    # pdb.set_trace()
-    tb.cells[4].source = tb.cells[4].source.replace("get_booking('./data')", "")
+    tb.cells[4].source = tb.cells[4].source.replace("get_booking('/workspace/data')", "")
     tb.cells[28].source = tb.cells[28].source.replace("d_model=64", "d_model=40")
     tb.cells[30].source = tb.cells[30].source.replace("epochs=5", "epochs=1")
     tb.execute()
