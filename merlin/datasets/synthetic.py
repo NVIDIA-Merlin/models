ee308ffab4f928733b1a01b817f008e8cc9bb775
Oliver Holworthy <oholworthy@nvidia.com>
2022-11-15 11:03:02 +0000

    Use `max_session_length` for sequence length of item id feature

--- merlin/datasets/synthetic.py
+++ merlin/datasets/synthetic.py
@@ -232,7 +232,7 @@ def generate_user_item_interactions(
     if not is_list_feature:
         shape = num_interactions
     else:
-        shape = (num_interactions, item_id_col.value_count.max)  # type: ignore
+        shape = (num_interactions, max_session_length)  # type: ignore
     tmp = _array.clip(
         _array.random.lognormal(3.0, 1.0, shape).astype(_array.int32),
         1,
