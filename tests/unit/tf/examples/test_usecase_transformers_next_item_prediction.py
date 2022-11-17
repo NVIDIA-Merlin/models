1eb62e778ff2607374ae8bfe61288593c4789e7d
Radek Osmulski <rosmulski@gmail.com>
2022-11-17 21:47:33 +1000

    T4rec use case (#853)
    * start working on next item prediction usecase
    
    * clean up notebook
    
    * update
    
    * update
    
    * update
    
    * update
    
    * switch to using BERT
    
    * update
    
    * add synthetic data & unit test
    
    * fix unit test
    
    * update
    
    * fix unit test; clean up
    
    * add schema
    
    * update
    
    * add tracking logo
    
    * use absolute path for data
    
    * fix unit test
    
    * fix unit test
    
    * fix paths in test
    
    * Add test for retrieval model with transformer block (#833)
    
    * Add test for transformer with RetrievalModelV2
    
    * Update test for transformer retrieval model
    
    * Remove test_retrieval from test_block
    
    * Allow index param to be optional to `Encoder.encode`
    
    * Correct target extraction in `SequencePredictNext`
    
    * Replace ragged coercion with axis aware tf.squeeze
    
    * Revert change to predict next
    
    * Remove unused ReplaceMaskedEmbeddings (only required for MLM model)
    
    * Support tuple return type from model.fit `pre` argument
    
    * Use predict last and use as pre instead of transform
    
    * Revert changes to contrastive output
    
    * Set process_lists default value to False
    
    * Add d_model and MLPBlock
    
    * Revert change to `Encoder.encode`
    
    * Revert change to default value of `process_lists` in `sample_batch`
    
    * Remove commented query_embeddings line
    
    * update comment about prediction tuple
    
    Co-authored-by: Marc Romeyn <marcromeyn@gmail.com>
    
    * Enable concat of sequence features with `InputBlockV2` (#883)
    
    * Add test for seuqence features concat
    
    * Update EmbeddingTable squeeze logic to handle ragged tensors
    
    * Update docs building actions (#884)
    
    - Update to avoid deprecation messages.
    
    - Delete the .doctrees and .buildinfo files
      from the sphinx-multiversion builds.
    
    * update
    
    * Do not copy source to docs output (#888)
    
    Set `html_copy_source` to `False` to
    avoid copying the documentation source
    files to the output. This should help
    reduce the size of the `gh-pages` branch.
    
    Also, run the common workflow that
    removes stale review directories. This should
    also reduce the size of `gh-pages` and the repo.
    
    * update
    
    * update
    
    * update
    
    Co-authored-by: rnyak <ronayak@hotmail.com>
    Co-authored-by: Oliver Holworthy <oholworthy@nvidia.com>
    Co-authored-by: Marc Romeyn <marcromeyn@gmail.com>
    Co-authored-by: mikemckiernan <mmckiernan@nvidia.com>
    Co-authored-by: bschifferer <bschifferer@nvidia.com>

---
+++ tests/unit/tf/examples/test_usecase_transformers_next_item_prediction.py
@@ -0,0 +1,31 @@
+from testbook import testbook
+
+from tests.conftest import REPO_ROOT
+
+
+@testbook(REPO_ROOT / "examples/usecases/transformers-next-item-prediction.ipynb", execute=False)
+def test_usecase_pretrained_embeddings(tb):
+    tb.inject(
+        """
+        import os, random
+        from datetime import datetime, timedelta
+        from merlin.datasets.synthetic import generate_data
+        ds = generate_data('booking.com-raw', 10000)
+        df = ds.compute()
+        def generate_date():
+            date = datetime.today()
+            if random.randint(0, 1):
+                date -= timedelta(days=7)
+            return date
+        df['checkin'] = [generate_date() for _ in range(df.shape[0])]
+        df['checkout'] = [generate_date() for _ in range(df.shape[0])]
+        df.to_csv('/tmp/train_set.csv')
+        """
+    )
+    tb.cells[4].source = tb.cells[4].source.replace("get_booking('/workspace/data')", "")
+    tb.cells[4].source = tb.cells[4].source.replace(
+        "read_csv('/workspace/data/train_set.csv'", "read_csv('/tmp/train_set.csv'"
+    )
+    tb.cells[28].source = tb.cells[28].source.replace("d_model=64", "d_model=40")
+    tb.cells[30].source = tb.cells[30].source.replace("epochs=5", "epochs=1")
+    tb.execute()
