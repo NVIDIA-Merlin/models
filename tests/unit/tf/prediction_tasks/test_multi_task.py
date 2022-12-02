0f27316acaa8aafbe8b13253c521dd4d3d9b4843
Gabriel Moreira <gmoreira@nvidia.com>
2022-12-01 22:19:45 -0300

    Implemented PLE model. Fixed CGC model to not share weights for experts and gates.

--- tests/unit/tf/prediction_tasks/test_multi_task.py
+++ tests/unit/tf/prediction_tasks/test_multi_task.py
@@ -3,7 +3,7 @@ from typing import Dict, Optional
 import pytest
 import tensorflow as tf
 
-import merlin.models.tf as ml
+import merlin.models.tf as mm
 from merlin.io import Dataset
 from merlin.models.tf.core.base import Block, PredictionOutput
 from merlin.models.tf.utils import testing_utils
@@ -14,24 +14,24 @@ from merlin.models.tf.utils import testing_utils
     "task_blocks",
     [
         None,
-        ml.MLPBlock([32]),
-        dict(click=ml.MLPBlock([16]), play_percentage=ml.MLPBlock([20])),
-        dict(binary_classification_task=ml.MLPBlock([16]), regression_task=ml.MLPBlock([20])),
+        mm.MLPBlock([32]),
+        dict(click=mm.MLPBlock([16]), play_percentage=mm.MLPBlock([20])),
+        dict(binary_classification_task=mm.MLPBlock([16]), regression_task=mm.MLPBlock([20])),
         {
-            "click/binary_classification_task": ml.MLPBlock([16]),
-            "play_percentage/regression_task": ml.MLPBlock([20]),
+            "click/binary_classification_task": mm.MLPBlock([16]),
+            "play_percentage/regression_task": mm.MLPBlock([20]),
         },
     ],
 )
 def test_model_with_multiple_tasks(music_streaming_data: Dataset, task_blocks, run_eagerly: bool):
     music_streaming_data.schema = music_streaming_data.schema.without("like")
 
-    inputs = ml.InputBlock(music_streaming_data.schema)
-    prediction_tasks = ml.PredictionTasks(music_streaming_data.schema, task_blocks=task_blocks)
-    model = ml.Model(inputs, ml.MLPBlock([64]), prediction_tasks)
+    inputs = mm.InputBlock(music_streaming_data.schema)
+    prediction_tasks = mm.PredictionTasks(music_streaming_data.schema, task_blocks=task_blocks)
+    model = mm.Model(inputs, mm.MLPBlock([64]), prediction_tasks)
     model.compile(optimizer="adam", run_eagerly=run_eagerly)
 
-    metrics = model.train_step(ml.sample_batch(music_streaming_data, batch_size=50))
+    metrics = model.train_step(mm.sample_batch(music_streaming_data, batch_size=50))
 
     assert metrics["loss"] >= 0
     assert set(list(metrics.keys())) == set(
@@ -55,7 +55,7 @@ def test_model_with_multiple_tasks(music_streaming_data: Dataset, task_blocks, r
 @testing_utils.mark_run_eagerly_modes
 @pytest.mark.parametrize(
     "task_blocks",
-    [None, ml.MLPBlock([32])],
+    [None, mm.MLPBlock([32])],
 )
 @pytest.mark.parametrize(
     "enable_gate_weights_metrics",
@@ -67,18 +67,18 @@ def test_mmoe_model(
     task_blocks: Optional[Block],
     enable_gate_weights_metrics: bool,
 ):
-    inputs = ml.InputBlockV2(music_streaming_data.schema)
-    prediction_tasks = ml.PredictionTasks(music_streaming_data.schema, task_blocks=task_blocks)
+    inputs = mm.InputBlockV2(music_streaming_data.schema)
+    prediction_tasks = mm.PredictionTasks(music_streaming_data.schema, task_blocks=task_blocks)
     num_experts = 4
-    mmoe = ml.MMOEBlock(
+    mmoe = mm.MMOEBlock(
         inputs,
         prediction_tasks,
-        expert_block=ml.MLPBlock([64]),
+        expert_block=mm.MLPBlock([64]),
         num_experts=num_experts,
-        gate_block=ml.MLPBlock([32]),
+        gate_block=mm.MLPBlock([32]),
         enable_gate_weights_metrics=enable_gate_weights_metrics,
     )
-    model = ml.Model(mmoe, prediction_tasks)
+    model = mm.Model(mmoe, prediction_tasks)
 
     loss_weights = {
         "click/binary_classification_task": 1.0,
@@ -88,7 +88,7 @@ def test_mmoe_model(
 
     model.compile(optimizer="adam", run_eagerly=run_eagerly, loss_weights=loss_weights)
 
-    metrics = model.train_step(ml.sample_batch(music_streaming_data, batch_size=50))
+    metrics = model.train_step(mm.sample_batch(music_streaming_data, batch_size=50))
 
     assert metrics["loss"] >= 0
 
@@ -136,12 +136,12 @@ def test_mmoe_block_task_specific_sample_weight_and_weighted_metrics(
             outputs = outputs.copy_with_updates(sample_weight=targets)
             return outputs
 
-    inputs = ml.InputBlockV2(music_streaming_data.schema)
-    prediction_tasks = ml.PredictionTasks(
+    inputs = mm.InputBlockV2(music_streaming_data.schema)
+    prediction_tasks = mm.PredictionTasks(
         music_streaming_data.schema, task_pre_dict={"like": CustomSampleWeight()}
     )
-    mmoe = ml.MMOEBlock(inputs, prediction_tasks, expert_block=ml.MLPBlock([64]), num_experts=4)
-    model = ml.Model(mmoe, prediction_tasks)
+    mmoe = mm.MMOEBlock(inputs, prediction_tasks, expert_block=mm.MLPBlock([64]), num_experts=4)
+    model = mm.Model(mmoe, prediction_tasks)
 
     loss_weights = {
         "click/binary_classification_task": 1.0,
@@ -174,7 +174,7 @@ def test_mmoe_block_task_specific_sample_weight_and_weighted_metrics(
         weighted_metrics=weighted_metrics,
     )
 
-    metrics = model.train_step(ml.sample_batch(music_streaming_data, batch_size=50))
+    metrics = model.train_step(mm.sample_batch(music_streaming_data, batch_size=50))
 
     assert metrics["loss"] >= 0
     assert set(metrics.keys()) == set(
@@ -210,18 +210,66 @@ def test_mmoe_block_task_specific_sample_weight_and_weighted_metrics(
 @testing_utils.mark_run_eagerly_modes
 @pytest.mark.parametrize(
     "task_blocks",
-    [None, ml.MLPBlock([32])],
+    [None, mm.MLPBlock([32])],
 )
 def test_cgc_model(music_streaming_data: Dataset, run_eagerly: bool, task_blocks: Optional[Block]):
-    inputs = ml.InputBlockV2(music_streaming_data.schema)
-    prediction_tasks = ml.PredictionTasks(music_streaming_data.schema, task_blocks=task_blocks)
-    cgc = ml.CGCBlock(
-        prediction_tasks, expert_block=ml.MLPBlock([64]), num_task_experts=2, num_shared_experts=3
+    schema = music_streaming_data.schema
+    inputs = mm.InputBlockV2(schema)
+    prediction_tasks = mm.PredictionTasks(schema, task_blocks=task_blocks)
+    cgc = mm.CGCBlock(
+        prediction_tasks,
+        expert_block=mm.MLPBlock([64]),
+        num_task_experts=2,
+        num_shared_experts=3,
+        schema=schema,
     )
-    model = ml.Model(inputs, cgc, prediction_tasks)
+    model = mm.Model(inputs, cgc, prediction_tasks)
     model.compile(optimizer="adam", run_eagerly=run_eagerly)
 
-    metrics = model.train_step(ml.sample_batch(music_streaming_data, batch_size=50))
+    metrics = model.train_step(mm.sample_batch(music_streaming_data, batch_size=50))
+
+    assert metrics["loss"] >= 0
+    assert set(metrics.keys()) == set(
+        [
+            "loss",
+            "loss_batch",
+            "click/binary_classification_task_loss",
+            "like/binary_classification_task_loss",
+            "play_percentage/regression_task_loss",
+            "click/binary_classification_task_precision",
+            "click/binary_classification_task_recall",
+            "click/binary_classification_task_binary_accuracy",
+            "click/binary_classification_task_auc",
+            "like/binary_classification_task_precision_1",
+            "like/binary_classification_task_recall_1",
+            "like/binary_classification_task_binary_accuracy",
+            "like/binary_classification_task_auc_1",
+            "play_percentage/regression_task_root_mean_squared_error",
+            "regularization_loss",
+        ]
+    )
+
+
+@testing_utils.mark_run_eagerly_modes
+@pytest.mark.parametrize(
+    "task_blocks",
+    [None, mm.MLPBlock([32])],
+)
+def test_ple_model(music_streaming_data: Dataset, run_eagerly: bool, task_blocks: Optional[Block]):
+    schema = music_streaming_data.schema
+    inputs = mm.InputBlockV2(music_streaming_data.schema)
+    prediction_tasks = mm.PredictionTasks(schema, task_blocks=task_blocks)
+    cgc = mm.PLEBlock(
+        num_layers=2,
+        outputs=prediction_tasks,
+        expert_block=mm.MLPBlock([64]),
+        num_task_experts=2,
+        num_shared_experts=3,
+    )
+    model = mm.Model(inputs, cgc, prediction_tasks)
+    model.compile(optimizer="adam", run_eagerly=run_eagerly)
+
+    metrics = model.train_step(mm.sample_batch(music_streaming_data, batch_size=50))
 
     assert metrics["loss"] >= 0
     assert set(metrics.keys()) == set(
