first attempts to fix inference and evaluation of causal transformer-based model

merlin/models/tf/models/base.py
@@ -817,13 +817,13 @@ class BaseModel(tf.keras.Model):
         predictions: Dict[str, TensorLike],
         targets: Optional[Union[tf.Tensor, Dict[str, tf.Tensor]]],
     ):
-        """Adjusts the predctions and targets, doing the following transformations
+        """Adjusts the predictions and targets, doing the following transformations
         if the target is provided:
         - Converts ragged targets (and their masks) to dense, so that they are compatible
         with most losses and metrics
         - Copies the targets mask to predictions mask, if defined
         - One-hot encode targets if their tf.rank(targets) == tf.rank(predictions)-1
-        - Ensures targets has the same shape and dtype as predicitnos
+        - Ensures targets have the same shape and dtype as predictions
 
         Parameters
         ----------
@@ -853,6 +853,35 @@ class BaseModel(tf.keras.Model):
 
             # Ensuring targets and preds have the same dtype
             targets[k] = tf.cast(targets[k], predictions[k].dtype)
+
+            # If targets are scalars (1-D) and predictions are sequential (3-D),
+            # extract predictions at target position because Keras expects
+            # predictions and targets to have the same shape.
+            def _extract_masked_predictions():
+                num_preds_per_example = tf.reduce_sum(
+                    tf.cast(predictions[k]._keras_mask, tf.int32), axis=-1
+                )
+                with tf.control_dependencies(
+                    [
+                        tf.debugging.assert_equal(
+                            num_preds_per_example,
+                            1,
+                            message="If targets are scalars (1-D) and predictions are"
+                            " sequential (3-D), the predictions mask should contain"
+                            " one masked position per example",
+                        )
+                    ]
+                ):
+                    return tf.boolean_mask(predictions[k], predictions[k]._keras_mask)
+
+            if getattr(predictions[k], "_keras_mask", None) is not None:
+                rank_check = tf.logical_and(
+                    tf.equal(tf.rank(targets[k]), 1),
+                    tf.equal(tf.rank(predictions[k]), 3),
+                )
+                predictions[k] = tf.cond(
+                    rank_check, _extract_masked_predictions, lambda: predictions[k]
+                )
 
             # Ensuring targets are one-hot encoded if they are not
             targets[k] = tf.cond(

merlin/models/tf/transformers/block.py
@@ -31,7 +31,10 @@ from transformers import (
 
 from merlin.models.tf.core import combinators
 from merlin.models.tf.core.base import Block, block_registry
-from merlin.models.tf.transformers.transforms import PrepareTransformerInputs
+from merlin.models.tf.transformers.transforms import (
+    PrepareTransformerInputs,
+    TransformerInferenceHiddenState,
+)
 from merlin.models.tf.utils.tf_utils import (
     maybe_deserialize_keras_objects,
     maybe_serialize_keras_objects,
@@ -99,6 +102,8 @@ class TransformerBlock(Block):
             transformer_post = block_registry.parse(transformer_post)
         self.transformer_post = transformer_post
 
+        self.inference_post = TransformerInferenceHiddenState()
+
         if isinstance(pre, str):
             pre = block_registry.parse(pre)
 
@@ -143,6 +148,7 @@ class TransformerBlock(Block):
     @property
     def to_call_post(self):
         yield self.transformer_post
+        yield self.inference_post
 
         if self.post:
             yield self.post

merlin/models/tf/transforms/sequence.py
@@ -661,7 +661,7 @@ class SequenceMaskLastInference(Block):
         return inputs
 
     def compute_mask(self, inputs, mask=None):
-        """Selects (masks) the nex position after the
+        """Selects (masks) the next position after the
         last valid (non-padded) position of the sequential targets
         to be predicted.
         This method is called by Keras after call()
@@ -682,6 +682,27 @@ class SequenceMaskLastInference(Block):
                 )
 
         return targets_mask
+
+
+@tf.keras.utils.register_keras_serializable(package="merlin.models")
+class SequenceCausalLastPosition(Block):
+    def compute_mask(self, inputs, mask=None):
+        """Selects (masks) the last non padded position of the
+        input sequence to be predicted.
+        This method is called by Keras after call()
+        and returns the mask that is going to be assigned
+        to the input tensors, being accessible
+        by tensor._keras_mask
+        """
+        if isinstance(inputs, tf.RaggedTensor):
+            row_lengths = inputs.row_lengths(1)
+            max_seq_length = tf.cast(tf.reduce_max(row_lengths), tf.int32)
+
+            padding_mask = tf.sequence_mask(row_lengths)
+            mask = tf.ragged.boolean_mask(
+                tf.cast(tf.one_hot(row_lengths - 1, max_seq_length), tf.bool), padding_mask
+            )
+        return mask
 
 
 @tf.keras.utils.register_keras_serializable(package="merlin.models")

tests/unit/tf/transformers/test_block.py
@@ -232,6 +232,7 @@ def classification_loader(sequence_testing_data: Dataset):
 
 @pytest.mark.parametrize("run_eagerly", [True, False])
 def test_transformer_with_causal_language_modeling(sequence_testing_data: Dataset, run_eagerly):
+    from merlin.models.tf.transforms.sequence import SequenceCausalLastPosition
 
     seq_schema = sequence_testing_data.schema.select_by_tag(Tags.SEQUENCE).select_by_tag(
         Tags.CATEGORICAL
@@ -254,7 +255,9 @@ def test_transformer_with_causal_language_modeling(sequence_testing_data: Datase
             ),
         ),
         mm.MLPBlock([transformer_input_dim]),
-        GPT2Block(d_model=transformer_input_dim, n_head=8, n_layer=2),
+        GPT2Block(
+            d_model=transformer_input_dim, n_head=8, n_layer=2, pre=SequenceCausalLastPosition()
+        ),
         mm.CategoricalOutput(
             model_schema.select_by_name(target), default_loss="categorical_crossentropy"
         ),
@@ -262,7 +265,7 @@ def test_transformer_with_causal_language_modeling(sequence_testing_data: Datase
 
     batch = next(iter(loader))[0]
     outputs = model(batch)
-    assert list(outputs.shape) == [8, 4, 51997]
+    assert list(outputs.shape) == [8, 51997]
     testing_utils.model_test(
         model, loader, run_eagerly=run_eagerly, reload_model=True, fit_kwargs={"pre": predict_next}
     )
@@ -271,7 +274,11 @@ def test_transformer_with_causal_language_modeling(sequence_testing_data: Datase
     assert len(metrics) > 0
 
     predictions = model.predict(loader, batch_size=8, steps=1)
-    assert predictions.shape == (8, 4, 51997)
+    assert predictions.shape == (8, 51997)
+
+    predict_last = mm.SequencePredictLast(schema=seq_schema, target=target)
+    metrics = model.evaluate(loader, batch_size=8, steps=1, return_dict=True, pre=predict_last)
+    assert len(metrics) > 0
 
 
 @pytest.mark.parametrize("run_eagerly", [True, False])
@@ -300,7 +307,6 @@ def test_transformer_with_masked_language_modeling(sequence_testing_data: Datase
             n_head=8,
             n_layer=2,
             pre=mm.SequentialBlock([mm.SequenceMaskLastInference(), mm.ReplaceMaskedEmbeddings()]),
-            post="inference_hidden_state",
         ),
         mm.CategoricalOutput(
             seq_schema.select_by_name(target),