d0afb9147e9cd1414b6bcd36ad3cb4701fe4ace9
Marc Romeyn <marcromeyn@gmail.com>
2023-03-20 11:11:08 +0100

    Add some fixes + tests for MLPBlock

---
+++ merlin/models/torch/blocks/mlp.py
@@ -0,0 +1,69 @@
+from typing import List, Optional, Sequence
+
+from torch import nn
+
+from merlin.models.torch.core.combinators import SequentialBlock
+
+
+class MLPBlock(SequentialBlock):
+    """
+    Multi-Layer Perceptron (MLP) Block with custom options for activation, normalization,
+    dropout, and pre/post layers.
+
+    Parameters
+    ----------
+    units : Sequence[int]
+        Sequence of integers specifying the dimensions of each linear layer.
+    activation : Callable, optional
+        Activation function to apply after each linear layer. Default is ReLU.
+    normalization : Union[str, nn.Module], optional
+        Normalization method to apply after the activation function.
+        Supported options are "batch_norm" or any custom `nn.Module`.
+        Default is None (no normalization).
+    dropout : Optional[float], optional
+        Dropout probability to apply after the normalization.
+        Default is None (no dropout).
+    pre : nn.Module, optional
+        An additional layer to add before the main MLP sequence.
+        Default is None (no pre-layer).
+    post : nn.Module, optional
+        An additional layer to add after the main MLP sequence.
+        Default is None (no post-layer).
+
+    Raises
+    ------
+    ValueError
+        If the normalization parameter is not supported.
+    """
+
+    def __init__(
+        self,
+        units: Sequence[int],
+        activation=nn.ReLU,
+        normalization=None,
+        dropout: Optional[float] = None,
+        pre=None,
+        post=None,
+    ):
+        modules: List[nn.Module] = []
+
+        for dim in units:
+            modules.append(nn.LazyLinear(dim))
+            if activation is not None:
+                modules.append(activation(inplace=True))
+
+            if normalization:
+                if normalization == "batch_norm":
+                    modules.append(nn.LazyBatchNorm1d())
+                elif isinstance(normalization, nn.Module):
+                    modules.append(normalization)
+                else:
+                    raise ValueError(f"Normalization {normalization} not supported")
+
+            if dropout:
+                if isinstance(dropout, nn.Module):
+                    modules.append(dropout)
+                else:
+                    modules.append(nn.Dropout(dropout))
+
+        super().__init__(*modules, pre=pre, post=post)
---
+++ tests/unit/torch/blocks/test_mlp.py
@@ -0,0 +1,71 @@
+import pytest
+from torch import nn
+
+from merlin.models.torch.blocks.mlp import MLPBlock
+from merlin.models.torch.core.combinators import SequentialBlock
+
+
+class TestMLPBlock:
+    def test_init(self):
+        units = [32, 64, 128]
+        mlp = MLPBlock(units)
+        assert isinstance(mlp, MLPBlock)
+        assert isinstance(mlp, SequentialBlock)
+        assert len(mlp) == len(units) * 2
+
+    def test_activation(self):
+        units = [32, 64, 128]
+        mlp = MLPBlock(units, activation=nn.ReLU)
+        assert isinstance(mlp, MLPBlock)
+        for i, module in enumerate(mlp):
+            if i % 2 == 1:
+                assert isinstance(module, nn.ReLU)
+
+    def test_normalization_batch_norm(self):
+        units = [32, 64, 128]
+        mlp = MLPBlock(units, normalization="batch_norm")
+        assert isinstance(mlp, MLPBlock)
+        for i, module in enumerate(mlp):
+            if (i + 1) % 3 == 0:
+                assert isinstance(module, nn.LazyBatchNorm1d)
+
+    def test_normalization_custom(self):
+        units = [32, 64, 128]
+        custom_norm = nn.LayerNorm(1)
+        mlp = MLPBlock(units, normalization=custom_norm)
+        assert isinstance(mlp, MLPBlock)
+        for i, module in enumerate(mlp):
+            if i % 3 == 2:
+                assert isinstance(module, nn.LayerNorm)
+
+    def test_normalization_invalid(self):
+        units = [32, 64, 128]
+        with pytest.raises(ValueError):
+            MLPBlock(units, normalization="invalid")
+
+    def test_dropout_float(self):
+        units = [32, 64, 128]
+        mlp = MLPBlock(units, dropout=0.5)
+        assert isinstance(mlp, MLPBlock)
+        for i, module in enumerate(mlp):
+            if i % 3 == 2:
+                assert isinstance(module, nn.Dropout)
+                assert module.p == 0.5
+
+    def test_dropout_module(self):
+        units = [32, 64, 128]
+        mlp = MLPBlock(units, dropout=nn.Dropout(0.5))
+        assert isinstance(mlp, MLPBlock)
+        for i, module in enumerate(mlp):
+            if i % 3 == 2:
+                assert isinstance(module, nn.Dropout)
+                assert module.p == 0.5
+
+    def test_pre_post(self):
+        units = [32, 64, 128]
+        pre = nn.Linear(1, 1)
+        post = nn.Linear(1, 1)
+        mlp = MLPBlock(units, pre=pre, post=post)
+        assert isinstance(mlp, MLPBlock)
+        assert mlp.pre == pre
+        assert mlp.post == post
