56c33d48a6970b835ae9600c9c7b0544f4ed1ad9
Marc Romeyn <marcromeyn@gmail.com>
2023-05-12 12:21:56 +0200

    Adding torch github-action + add copyright

--- merlin/models/torch/__init__.py
+++ merlin/models/torch/__init__.py
@@ -13,6 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+<<<<<<< HEAD
+=======
+
+from merlin.models.torch.batch import Batch
+from merlin.models.torch.block import Block
+>>>>>>> 2dfe2782 (Adding torch github-action + add copyright)
 
 from merlin.models.torch.batch import Batch, Sequence
 from merlin.models.torch.block import Block, ParallelBlock
