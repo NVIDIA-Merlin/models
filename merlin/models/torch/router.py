1d904ef7a8360053494028a4b5139c83a80419a0
Marc Romeyn <marcromeyn@gmail.com>
2023-05-17 09:08:21 +0200

    Some tiny doc-string updates

--- merlin/models/torch/router.py
+++ merlin/models/torch/router.py
@@ -98,6 +98,7 @@ class RouterBlock(ParallelBlock, Selectable):
         """Add a new route for each column in a selection.
 
         Example usage::
+
             router.add_route_for_each(Tags.EMBEDDING, mm.MLPBlock([64, 32]]))
 
         Parameters
@@ -199,6 +200,7 @@ class SelectKeys(nn.Module, Selectable):
     """Filter tabular data based on a defined schema.
 
     Example usage::
+
         >>> select_keys = mm.SelectKeys(Schema(["user_id", "item_id"]))
         >>> inputs = {
         ...     "user_id": torch.tensor([1, 2, 3]),
