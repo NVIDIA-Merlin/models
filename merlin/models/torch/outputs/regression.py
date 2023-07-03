8dd234e4a1634bb7dd0a8189f263673e60b913f0
Marc Romeyn <marcromeyn@gmail.com>
2023-07-03 14:16:13 +0200

    Remove default_metrics method from RegressionOutput

--- merlin/models/torch/outputs/regression.py
+++ merlin/models/torch/outputs/regression.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from typing import List, Optional, Sequence, Union
+from typing import Optional, Sequence, Union
 
 from torch import nn
 from torchmetrics import MeanSquaredError, Metric
@@ -74,8 +74,3 @@ class RegressionOutput(ModelOutput):
         _target = target.with_dtype(md.float32).with_tags([Tags.CONTINUOUS])
 
         self.output_schema = Schema([_target])
-
-    def default_metrics(self) -> List[Metric]:
-        return [
-            MeanSquaredError(),
-        ]
