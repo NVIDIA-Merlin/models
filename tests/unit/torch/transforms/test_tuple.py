c9091af34a8126163ef99a2cdc48e5382e36e8c7
Marc Romeyn <marcromeyn@gmail.com>
2023-07-05 10:27:01 +0200

    Adding ToTuple function that uses the length of the schema to get the right ToTupleModule

--- tests/unit/torch/transforms/test_tuple.py
+++ tests/unit/torch/transforms/test_tuple.py
@@ -10,7 +10,8 @@ class TestToTuple:
     @pytest.mark.parametrize("length", [i + 1 for i in range(10)])
     def test_with_length(self, length):
         schema = Schema([str(i) for i in range(length)])
-        to_tuple = getattr(tuple, f"ToTuple{length}")(schema)
+        to_tuple = tuple.ToTuple(schema)
+        assert isinstance(to_tuple, getattr(tuple, f"ToTuple{length}"))
 
         inputs = {str(i): torch.randn(2, 3) for i in range(length)}
         outputs = module_utils.module_test(to_tuple, inputs)
@@ -18,8 +19,10 @@ class TestToTuple:
         assert len(outputs) == length
 
     def test_exception(self):
-        to_tuple = tuple.ToTuple2()
+        with pytest.raises(ValueError):
+            tuple.ToTuple(Schema([str(i) for i in range(11)]))
 
+        to_tuple = tuple.ToTuple2()
         inputs = {"0": torch.randn(2, 3), "1": torch.randn(2, 3)}
         with pytest.raises(RuntimeError):
             module_utils.module_test(to_tuple, inputs)
