8f308e97e8e40e379a676e0774869a20b6171be0
edknv <edwardk@nvidia.com>
2023-07-10 05:03:47 -0700

    test rank

--- tests/integration/torch/test_multi_gpu.py
+++ tests/integration/torch/test_multi_gpu.py
@@ -15,9 +15,12 @@ class TestMultiGPU:
             mm.BinaryOutput(schema["click"]),
         )
 
-        trainer = pl.Trainer(max_epochs=3, devices=2)
-        trainer.fit(model, mm.MultiLoader(data, batch_size=2, repartition=2))
+        trainer = pl.Trainer(max_epochs=3, devices=[0, 1])
+        multi_loader = mm.MultiLoader(data, batch_size=2)
+        trainer.fit(model, multi_loader)
 
         # 100 rows total / 2 devices -> 50 rows per device
         # 50 rows / 2 per batch -> 25 steps per device
         assert trainer.num_training_batches == 25
+
+        assert trainer.global_rank == 0  # This should fail for node 1.
