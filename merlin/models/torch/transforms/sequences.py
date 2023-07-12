91160faa79c0ec1b673bb5db310c3ad26a4121f6
sararb <sara.rabhi@gmail.com>
2023-07-12 16:38:40 +0000

    first impelementation of tabular predict next

--- merlin/models/torch/transforms/sequences.py
+++ merlin/models/torch/transforms/sequences.py
@@ -338,3 +338,179 @@ class BroadcastToSequence(nn.Module, LazySchemaModuleMixin):
             return inputs[first_feat + "__offsets"][-1].item()
 
         return inputs[first_feat].shape[1]
+
+
+class TabularPredictNext(BatchBlock):
+    """A Batchblock instance for preparing sequential inputs and targets
+    for next-item prediction. The target is extracted from the shifted
+    sequence of the target feature and the sequential input features
+    are truncated in the last position.
+
+    Parameters
+    ----------
+    target : Union[str, List[str], Tags]
+        The sequential input column(s) that will be used to extract the target.
+        Targets can be one or multiple input features with the same sequence length.
+    schema : Optional[Schema]
+        The schema with the sequential columns to be truncated
+    apply_padding : Optional[bool], default=True
+        Whether to pad sequential inputs before extracting the target(s).
+    max_sequence_length : Optional[int], default=None
+        The maximum length of the sequences after padding.
+        If None, sequences will be padded to the maximum length in the current batch.
+
+    Example usage::
+        batch_output = transform(batch)
+
+        features = {
+            'feature1': torch.tensor([[4, 3], [5, 2]),
+            'feature2': torch.tensor([[3,8], [7,9]])
+        }
+        schema = Schema(["feature1", "feature2"])
+        next_item_op = TabularPredictNext(
+            schema=schema, target='feature1'
+        )
+        transformed_batch = next_item_op(Batch(features))
+    """
+
+    def __init__(
+        self,
+        target: Union[str, List[str], Tags],
+        schema: Optional[Schema] = None,
+        apply_padding: bool = True,
+        max_sequence_length: int = None,
+        name: Optional[str] = None,
+    ):
+        super().__init__(
+            TabularPredictNextModule(
+                schema=schema,
+                target=target,
+                apply_padding=apply_padding,
+                max_sequence_length=max_sequence_length,
+            ),
+            name=name,
+        )
+
+
+class TabularSequenceTransform(nn.Module):
+    """Base PyTorch module for preparing targets from a batch of sequential inputs.
+    Parameters
+    ----------
+    target : Selection
+        The sequential input column that will be used to extract the target.
+        For multiple targets usecase, one should provide a Schema containing
+        all target columns.
+    schema : Optional[Schema]
+        The schema with the sequential columns to be truncated
+    apply_padding : Optional[bool], default=True
+        Whether to pad sequential inputs before extracting the target(s).
+    max_sequence_length : Optional[int], default=None
+        The maximum length of the sequences after padding.
+        If None, sequences will be padded to the maximum length in the current batch.
+    """
+
+    def __init__(
+        self,
+        target: Selection,
+        schema: Optional[Schema] = None,
+        apply_padding: bool = True,
+        max_sequence_length: int = None,
+    ):
+        super().__init__()
+        if schema:
+            self.initialize_from_schema(schema)
+            self._initialized_from_schema = True
+        self.target = select(self.schema, target)
+        self.target_name = self._get_target(self.target)
+        self.padding_idx = 0
+        self.apply_padding = apply_padding
+        if self.apply_padding:
+            self.padding_operator = TabularPadding(
+                schema=self.schema, max_sequence_length=max_sequence_length
+            )
+
+    def initialize_from_schema(self, schema: Schema):
+        self.schema = schema
+        self.features: List[str] = self.schema.column_names
+
+    def _get_target(self, target):
+        return target.column_names
+
+    def forward(
+        self, inputs: Union[torch.Tensor, Dict[str, torch.Tensor]], batch: Batch, **kwargs
+    ) -> Batch:
+        raise NotImplementedError()
+
+    def _check_seq_inputs_targets(self, batch: Batch):
+        self._check_input_sequence_lengths(batch)
+        self._check_target_shape(batch)
+
+    def _check_target_shape(self, batch):
+        for name in self.target_name:
+            if name not in batch.features:
+                raise ValueError(f"Inputs features do not contain target column ({name})")
+
+            target = batch.features[name]
+            if target.ndim < 2:
+                raise ValueError(
+                    f"Sequential target column ({name}) "
+                    f"must be a 2D tensor, but shape is {target.ndim}"
+                )
+            lengths = batch.sequences.length(name)
+            if any(lengths <= 1):
+                raise ValueError(
+                    f"2nd dim of target column ({name})"
+                    "must be greater than 1 for sequential input to be shifted as target"
+                )
+
+    def _check_input_sequence_lengths(self, batch):
+        if not batch.sequences.lengths:
+            raise ValueError(
+                "The input `batch` should include information about input sequences lengths"
+            )
+        sequence_lengths = torch.stack([batch.sequences.length(name) for name in self.features])
+        assert torch.all(sequence_lengths.eq(sequence_lengths[0])), (
+            "All tabular sequence features need to have the same sequence length, "
+            f"found {sequence_lengths}"
+        )
+
+
+class TabularPredictNextModule(TabularSequenceTransform):
+    """A PyTorch module for preparing tabular sequence data for next-item prediction."""
+
+    def forward(self, inputs: Union[torch.Tensor, Dict[str, torch.Tensor]], batch: Batch) -> Batch:
+        if self.apply_padding:
+            batch = self.padding_operator(batch)
+        self._check_seq_inputs_targets(batch)
+
+        # Shifts the target column to be the next item of corresponding input column
+        new_targets = {}
+        for name in self.target_name:
+            new_target = batch.features[name]
+            new_target = new_target[:, 1:]
+            new_targets[name] = new_target
+
+        # Removes the last item of the sequence, as it belongs to the target
+        new_inputs = dict()
+        for k, v in batch.features.items():
+            if k in self.features:
+                new_inputs[k] = v[:, :-1]
+
+        # Generates information about the new lengths and causal masks
+        new_lengths, causal_masks = {}, {}
+        _max_length = new_target.shape[-1]  # all new targets have same output sequence length
+        for name in self.features:
+            new_lengths[name] = batch.sequences.lengths[name] - 1
+            causal_masks[name] = self._generate_causal_mask(new_lengths[name], _max_length)
+
+        return Batch(
+            features=new_inputs,
+            targets=new_targets,
+            sequences=Sequence(new_lengths, masks=causal_masks),
+        )
+
+    def _generate_causal_mask(self, seq_lengths, max_len):
+        """
+        Generate a 2D mask from a tensor of sequence lengths.
+        """
+        return torch.arange(max_len)[None, :] < seq_lengths[:, None]
